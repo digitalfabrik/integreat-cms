c07f1ab6cd53200d74576f6e16fbd4aaf5f240b2
Timo Ludwig <ti.ludwig@web.de>
2019-01-06 14:05:44 +0100

    fix build error & remove duplicate code

--- backend/cms/models/postal_code_extra.py
+++ backend/cms/models/postal_code_extra.py
@@ -1,21 +1,7 @@
 from cms.models.extra import Extra
-from cms.models.extra_template import ExtraTemplate
 
 
 class PostalCodeExtra(Extra):
-    template = models.ForeignKey(ExtraTemplate)
-
-    def alias(self):
-        return self.template.alias
-
-    def name(self):
-        return self.template.name
-
-    def thumbnail(self):
-        return self.template.thumbnail
 
     def url(self):
         return self.template.url + self.site.postal_code
-
-    def post_data(self):
-        return self.template.post_data
