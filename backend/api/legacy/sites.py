f8b871a0379ed835045c430b7cf7d26c5db9f34d
Michael Markl <marklmichael98@gmail.com>
2019-01-15 14:34:37 +0100

    Add sites endpoint

--- backend/api/legacy/sites.py
+++ backend/api/legacy/sites.py
@@ -1,5 +1,54 @@
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
+
+from cms.models import Site
+
+PREFIXES = [
+    'EAE',
+    'Landkreis',
+    'Kreis',
+    'Stadt',
+]
 
 
-def sites(request):
-    return HttpResponse("Hello, world. You're at the API index.")
+def sites(_):
+    def strip_prefix(title):
+        for p in PREFIXES:
+            if title.startswith(p):
+                return p, title[len(p) + 1:]  # +1 for one whitespace
+        return None, title
+
+    def transform_site(s):
+        prefix, name_without_prefix = strip_prefix(s.title)
+        return {
+            'id': s.name,
+            'name': s.title,
+            'path': f'/{s.name}/',  # todo: tbd
+            'live': s.status == Site.ACTIVE,
+            'prefix': prefix,
+            'name_without_prefix': name_without_prefix,
+            'plz': s.postal_code,
+            'extras': s.extras_enabled,
+            'events': s.events_enabled,
+            'push-notifications': s.push_notifications_enabled,
+            'longitude': s.longitude,
+            'langitude': s.latitude,
+            'aliases': None  # todo
+        }
+
+    result = list(map(transform_site, Site.objects.exclude(status=Site.ARCHIVED)))
+    return JsonResponse(result, safe=False)  # Turn off Safe-Mode to allow serializing arrays
+
+
+def pushnew(_):
+    """
+    This is a convenience function for development.
+    todo: To be removed on deploy.
+    """
+    site = Site()
+    site.push_notification_channels = []
+    site.latitude = 48.37154
+    site.longitude = 10.89851
+    site.name = 'augsburg'
+    site.title = 'Stadt Augsburg'
+    site.save()
+    return HttpResponse('Pushing successful')
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import url
-from django.conf.urls.static import static
-from django.conf import settings
-from django.contrib.auth import views as auth_views
 
-from . import views
-from .legacy import sites
+from .legacy.sites import sites, pushnew
 
 urlpatterns = [
     url(r'sites/$', sites, name='sites'),
+    url(r'sites/pushnew/$', pushnew, name='pushnew')
 ]
