755b17f7a2b3573e6f6327e9b2fec97251dcf048
Michael Markl <marklmichael98@gmail.com>
2019-01-22 11:16:27 +0100

    Use bcp47 for language codes

--- backend/cms/models/language.py
+++ backend/cms/models/language.py
@@ -9,8 +9,11 @@ class Language(models.Model):
         ('rtl', 'right-to-left')
     )
 
-    code = models.CharField(max_length=2, primary_key=True, validators=[MinLengthValidator(2)])
+    # We use bcp47 language codes (see https://tools.ietf.org/html/bcp47).
+    # The recommended minimum buffer is 35 (see https://tools.ietf.org/html/bcp47#section-4.4.1).
+    # It's unlikely that we have language codes longer than 8 characters though.
+    code = models.CharField(max_length=8, primary_key=True, validators=[MinLengthValidator(2)])
     title = models.CharField(max_length=250, blank=False)
     text_direction = models.CharField(choices=DIRECTION, max_length=3)
-    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
