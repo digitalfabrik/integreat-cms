5e2707f3f0d165d3e0e83ee0005512069f8b9170
Timo Ludwig <ti.ludwig@web.de>
2019-06-04 21:49:14 +0200

    Add push notification views (resolves #33)

--- backend/cms/models/push_notification.py
+++ backend/cms/models/push_notification.py
@@ -13,11 +13,17 @@ class PushNotification(models.Model):
     Args:
         models : Databas model inherit from the standard django models
     """
-    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    site = models.ForeignKey(Site, related_name='push_notifications', on_delete=models.CASCADE)
     channel = models.CharField(max_length=60)
     draft = models.BooleanField(default=True)
+    sent_date = models.DateTimeField(null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        if self.translations.exists():
+            return self.translations.first().title
+        return ""
 
 
 class PushNotificationTranslation(models.Model):
@@ -29,6 +35,9 @@ class PushNotificationTranslation(models.Model):
     title = models.CharField(max_length=250)
     text = models.CharField(max_length=250)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
-    push_notification = models.ForeignKey(PushNotification, on_delete=models.CASCADE)
+    push_notification = models.ForeignKey(PushNotification, related_name='translations', on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
     last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
