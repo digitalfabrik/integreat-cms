8c10a12f7cbe767217696e817bcb9d195afaebd6
jshere <jshere@expediagroup.com>
2019-06-06 17:58:50 +1000

    Added v3 api endpoint to return notifications that have been sent

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import include, url
 
 from .v3.languages import languages
 from .v3.sites import sites, pushnew
+from .v3.push_notifications import sent_push_notifications
 
 urlpatterns = [
     url(r'sites/$', sites, name='sites'),
     url(r'sites/pushnew/$', pushnew, name='pushnew'),
+    url(r'sent_push_notifications/$', sent_push_notifications, name='sent_push_notifications'),
     url(r'(?P<site_slug>[-\w]+)/', include([
         url(r'languages$', languages),
     ])),
---
+++ backend/api/v3/push_notifications.py
@@ -0,0 +1,21 @@
+from django.http import JsonResponse
+
+from cms.models import PushNotificationTranslation
+
+"""
+Retrieve push notifications that have been sent, optionally filtering by channel.
+"""
+
+
+def sent_push_notifications(req):
+    channel = req.GET.get('channel', 'all')
+    query_result = PushNotificationTranslation.objects.filter(push_notification__sent_date__isnull=False)
+    if channel != 'all':
+        query_result = query_result.filter(push_notification__channel=channel)
+    result = list(map(transform_notification, query_result))
+    return JsonResponse(result, safe=False)
+
+
+def transform_notification(pn):
+    return {'title': pn.title, 'text': pn.text, 'channel': pn.push_notification.channel,
+            'sent_date': pn.push_notification.sent_date}
