af6178ccb9653ca55f5f3afa20f74bad0a80c6d6
Yanfei <yxue@expedia.com>
2019-06-06 16:29:47 +0800

    API: sites, languages
    - Django #16, sites
    - Django #18, languages

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 
 from .v3.languages import languages
-from .v3.sites import sites, pushnew
+from .v3.sites import sites, livesites, hiddenites, pushnew
 
 urlpatterns = [
     url(r'sites/$', sites, name='sites'),
+    url(r'sites/live/$', livesites, name='livesites'),
+    url(r'sites/hidden/$', hiddenites, name='hiddensites'),
     url(r'sites/pushnew/$', pushnew, name='pushnew'),
     url(r'(?P<site_slug>[-\w]+)/', include([
         url(r'languages$', languages),
