8e3566b18e39f4be76f250730177730d532744d8
Sven Seeberg <mail@sven-seeberg.de>
2019-08-13 17:01:25 +0200

    Upgrade Django version to 2.2 (LTS)
    - Remove fixed versions from setup.py
      * Remove fixed versions from pip packages during
        development process. Fixed versions will be
        reintroduced later, close to release.
    - Change extra class for Django 2
      * Add on_delete parameters for Foreign Keys.
      * Add module and class doc strings.
      * Disable pylint errors where applicable.
    - Change Feedback classes for Django 2
      * Add on_delete parameters for Foreign Keys.
      * Add module and class doc strings.
      * Disable pylint errors where applicable.
    - Fix error handlers
      * Add exception parameter to 4xx handlers.
      * Fix importing of modules.
    - Adapt view decorators to Django 2.2
    - Remove fixed versions for dev libraries

--- backend/cms/decorators.py
+++ backend/cms/decorators.py
@@ -1,21 +1,23 @@
+from functools import wraps
+
 from django.core.exceptions import PermissionDenied
 
 from .models import Region
 
 
 def staff_required(function):
+    @wraps(function)
     def wrap(request, *args, **kwargs):
         user = request.user
         # superusers and staff have access to this areas
         if user.is_superuser or user.is_staff:
             return function(request, *args, **kwargs)
         raise PermissionDenied
-    wrap.__doc__ = function.__doc__
-    wrap.__name__ = function.__name__
     return wrap
 
 
 def region_permission_required(function):
+    @wraps(function)
     def wrap(request, *args, **kwargs):
         user = request.user
         # superusers and staff have permissions for all regions
@@ -25,6 +27,4 @@ def region_permission_required(function):
         if region in user.profile.regions.all():
             return function(request, *args, **kwargs)
         raise PermissionDenied
-    wrap.__doc__ = function.__doc__
-    wrap.__name__ = function.__name__
     return wrap
--- backend/cms/models/feedback.py
+++ backend/cms/models/feedback.py
@@ -1,3 +1,6 @@
+"""
+Module for models storing feedback from front end users
+"""
 from django.db import models
 from cms.models.page import Page
 from cms.models.region import Region
@@ -6,6 +9,9 @@ from cms.models.event import Event
 
 
 class Feedback(models.Model):
+    """
+    Base class for collecting feeedback from users.
+    """
     EMOTION = (
         ("Pos", "Positive"),
         ("Neg", "Negative"),
@@ -18,6 +24,7 @@ class Feedback(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
+    # pylint: disable=R0903,C0111
     class Meta:
         default_permissions = ()
         permissions = (
@@ -26,48 +33,66 @@ class Feedback(models.Model):
 
 
 class RegionFeedback(Feedback):
+    """
+    General feedback for regions
+    """
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
 
-    region = models.ForeignKey(Region)
-
+    # pylint: disable=R0903,C0111
     class Meta:
         default_permissions = ()
 
 
 class PageFeedback(Feedback):
+    """
+    Feedback on a specific page
+    """
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
 
-    page = models.ForeignKey(Page)
-
+    # pylint: disable=R0903,C0111
     class Meta:
         default_permissions = ()
 
 
 class TechnicalFeedback(Feedback):
+    """
+    Technical feedback on the end user app
+    """
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
 
-    page = models.ForeignKey(Page)
-
+    # pylint: disable=R0903,C0111
     class Meta:
         default_permissions = ()
 
 
 class ExtraFeedback(Feedback):
+    """
+    Feedback on extras (extra model)
+    """
+    extra = models.ForeignKey(Extra, on_delete=models.CASCADE)
 
-    extra = models.ForeignKey(Extra)
-
+    # pylint: disable=R0903,C0111
     class Meta:
         default_permissions = ()
 
 
 class EventFeedback(Feedback):
+    """
+    Feedback on single events
+    """
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
-    event = models.ForeignKey(Event)
-
+    # pylint: disable=R0903,C0111
     class Meta:
         default_permissions = ()
 
 
 class SearchResultFeedback(Feedback):
-
+    """
+    Feedback on (i.e. empty) search results
+    """
     searchQuery = models.CharField(max_length=1000)
 
+    # pylint: disable=R0903,C0111
     class Meta:
         default_permissions = ()
