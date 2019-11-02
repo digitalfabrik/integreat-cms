4a64d9a6f41d327590fc49441961d089061dfdc6
Timo Ludwig <ludwig+secure@integreat-app.de>
2019-11-02 16:35:12 +0100

    Add translations and pylint to dev-tools
    - translations.sh re-generates and compiles the translation file
    - pylint.sh runs pylint_runner in the correct directory

---
+++ dev-tools/pylint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script can be used to run the pylint_runner while ignoring migrations.
+
+cd $(dirname "$BASH_SOURCE")/../backend
+
+# Activate venv
+source ../.venv/bin/activate
+
+# Run pylint
+pylint_runner
