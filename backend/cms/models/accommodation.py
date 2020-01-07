6965640f7440a9dd68f469010e26f9059cbcc18d
Tailoric <eric.schneider96@gmail.com>
2020-01-07 16:44:28 +0100

    Add Add Accommodation model (fixes #1)
    Creates a new subclass of the POI model specifically for cold aid.
    Extends POI by inheritance.

--- backend/cms/models/accommodation.py
+++ backend/cms/models/accommodation.py
@@ -1,8 +1,27 @@
-"""
-Dummy-model for the Bed's ForeignKey
-"""
+from django.db import models
 
+from .language import Language
+from .organization import Organization
 from .poi import POI
 
+
 class Accommodation(POI):
-    pass
+    """
+    A special kind of POI which is used for the cold aid functionality
+    """
+    institution = models.ForeignKey(Organization, null=False, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=12, null=False)
+    mobile_number = models.CharField(max_length=12, null=False)
+    wc_available = models.BooleanField(default=False)
+    shower_available = models.BooleanField(default=False)
+    animals_allowed = models.BooleanField(default=False)
+    intoxicated_allowed = models.BooleanField()
+    spoken_languages = models.ManyToManyField(Language)
+    intake_from = models.TimeField()
+    intake_to = models.TimeField()
+
+    class Meta:
+        default_permissions = ()
+        permissions = (
+            ('manage_accommodations', 'can manage accommodations'),
+        )
