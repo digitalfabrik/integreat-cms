c1d1c9c9ac88cfbae210e16c4858bf7386dc3789
Thomas Weizenegger <thomas.weiz97@gmail.com>
2020-04-09 11:34:15 +0200

    Correct url patterns

--- src/federation/urls.py
+++ src/federation/urls.py
@@ -8,13 +8,13 @@ from . import (
 from .utils import activate_federation_feature
 
 urlpatterns = [
-    url(r'^cms-domains/', access_point.cms_domains),
-    url(r'^cms-data/', access_point.cms_data),
-    url(r'^offer/', access_point.receive_offer),
-    url(r'^test/', tests.test), #todo: remove test-stuff
-    url(r'test-send-offer', tests.test_send_offer),
-    url(r'test-update', tests.test_update),
-    url(r'test-ask', tests.test_ask)
+    url(r'^cms-domains/$', access_point.cms_domains),
+    url(r'^cms-data/$', access_point.cms_data),
+    url(r'^offer/$', access_point.receive_offer),
+    url(r'^test/$', tests.test), #todo: remove test-stuff
+    url(r'test-send-offer/$', tests.test_send_offer),
+    url(r'test-update/$', tests.test_update),
+    url(r'test-ask/$', tests.test_ask)
 ]
 
 try:
