8aacad0aa1987a454d3d8d49ab6be4a2c72f7e8f
Timo Ludwig <ludwig+secure@integreat-app.de>
2020-04-11 15:13:41 +0200

    Improve readability of documentation (fixes #351)
    - Remove unnecessary "submodule"- & "subpackage"-headings
    - Remove unnecessary "module"- & "package"-strings at the end of headings
    - Do not show whole module path in headings
    - Do not collapse the navigation
    - Do not show autogenerated and undocumented fields
    - Add links to source code in GitHub to all class & function definitions
    - Add intersphinx mapping to Django documentation for model fields
    - Open external links in new tab
    - Increase `max-width` of content div

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -6,72 +6,135 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
 import os
 import sys
 import django
-sys.path.insert(0, os.path.abspath('../src/'))
+import inspect
+import importlib
+
+from sphinx.writers.html import HTMLTranslator
+
+# Append project source directory to path environment variable
+sys.path.append(os.path.abspath('../src/'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
+
+from backend.settings import VERSION
+
+# Setup Django
 django.setup()
 
+
+def setup(app):
+    # Register the docstring processor with sphinx to improve the appearance of Django models
+    app.connect('autodoc-process-docstring', process_django_models)
+    # Patch HTMLTranslator to open external links in new tab
+    app.set_translator('html', PatchedHTMLTranslator)
+
+
 # -- Project information -----------------------------------------------------
+
 
 project = 'integreat-cms'
 copyright = '2020, Integreat'
 author = 'Integreat'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
-
+release = VERSION
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# All enabled sphinx extensions
 extensions = [
-        'sphinx.ext.autodoc',
-        'sphinx.ext.coverage',
-        'sphinxcontrib_django',
-        'sphinx_rtd_theme',
-        ]
+    'sphinx.ext.autodoc',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.linkcode',
+    'sphinxcontrib_django',
+    'sphinx_rtd_theme',
+    ]
 
+# Enable cross-references to other documentations
 intersphinx_mapping = {
-        'http://docs.python.org/': None,
-        'https://docs.djangoproject.com/en/stable': 'https://docs.djangoproject.com/en/stable/_objects',
+    'python': ('https://docs.python.org/3.5', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    'django': ('https://docs.djangoproject.com/en/2.2/', 'https://docs.djangoproject.com/en/2.2/_objects/'),
 }
-# Add any paths that contain templates here, relative to this directory.
-# templates_path = ['templates']
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-# exclude_patterns = []
 
-# Do not include source in build
-html_show_sourcelink = False
+# The path for patched template files
+templates_path = ['templates']
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+# The theme to use for HTML and HTML Help pages.
 html_theme = 'sphinx_rtd_theme'
+# Do not show the project name, only the logo
 html_theme_options = {
-        'logo_only': True
+    'logo_only': True,
+    'collapse_navigation': False,
 }
-
-html_favicon = '../src/cms/static/images/favicon.ico'
+# The logo shown in the menu bar
 html_logo = '../src/cms/static/images/integreat-logo-white.png'
+# The facivon of the html doc files
+html_favicon = '../src/cms/static/images/favicon.ico'
+# The url where the docs should be published (via gh-pages)
+html_baseurl = 'https://Integreat.github.io/cms-django/'
+# Do not include links to the documentation source (.rst files) in build
+html_show_sourcelink = False
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['static']
+# -- Modify default Django model parameter types------------------------------
 
+
+def process_django_models(app, what, name, obj, options, lines):
+    """Append correct param types from fields to model documentation."""
+    if inspect.isclass(obj) and issubclass(obj, django.db.models.Model):
+        for field in obj._meta.fields:
+            field_type = type(field)
+            module = field_type.__module__
+            # Fix intersphinx mappings for django.contrib.postgres fields
+            if module == 'django.contrib.postgres.fields.array':
+                lines.append(':type {}: `django.contrib.postgres.fields.array.ArrayField <https://docs.djangoproject.com/en/3.0/ref/contrib/postgres/fields/#arrayfield>`_'.format(field.attname))
+                continue
+            elif module == 'django.contrib.postgres.fields.jsonb':
+                lines.append(':type {}: `django.contrib.postgres.fields.jsonb.JSONField <https://docs.djangoproject.com/en/3.0/ref/contrib/postgres/fields/#jsonfield>`_'.format(field.attname))
+                continue
+            elif 'django.db.models' in module:
+                # scope with django.db.models * imports
+                module = 'django.db.models'
+            lines.append(':type {}: {}.{}'.format(field.attname, module, field_type.__name__))
+    return lines
+
+
+# -- Source Code links to GitHub ---------------------------------------------
+
+
+def linkcode_resolve(domain, info):
+    """Link source code to GitHub."""
+    if domain != 'py' or not info['module']:
+        return None
+    filename = info['module'].replace('.', '/')
+    module = importlib.import_module(info['module'])
+    basename = os.path.splitext(module.__file__)[0]
+    if basename.endswith('__init__'):
+        filename += '/__init__'
+    item = module
+    line_number_reference = ''
+    for piece in info['fullname'].split('.'):
+        item = getattr(item, piece)
+        try:
+            line_number_reference = '#L{}'.format(inspect.getsourcelines(item)[1])
+        except (TypeError, IOError):
+            pass
+    return "https://github.com/Integreat/cms-django/blob/develop/src/{}.py{}".format(
+        filename,
+        line_number_reference
+    )
+
+# -- Link targets ------------------------------------------------------------
+
+
+class PatchedHTMLTranslator(HTMLTranslator):
+    """Open external links in a new tab"""
+    def visit_reference(self, node):
+        if node.get('newtab') or not (node.get('target') or node.get('internal') or 'refuri' not in node):
+            node['target'] = '_blank'
+        super().visit_reference(node)
