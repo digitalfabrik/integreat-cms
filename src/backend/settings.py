5a1d7d48fae6233fda8413451afba1ebe4e556e2
thimmy687 <kaser23@gmx.de>
2020-04-21 16:37:28 +0200

    Add custom development header to CORS config
    Signed-off-by: thimmy687 <kaser23@gmx.de>

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
 import os
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -245,3 +245,8 @@ GVZ_API_ENABLED = True
 
 # Allow access to all domains by setting the following variable to TRUE
 CORS_ORIGIN_ALLOW_ALL = True
+
+# Extend default headers with development header to differenciate dev traffic in statistics
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'x-integreat-development',
+]
