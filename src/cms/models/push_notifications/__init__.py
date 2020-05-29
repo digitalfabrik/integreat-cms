067cc7d301df7c1a9e26f2fe0c7a108394f06242
Timo Ludwig <ludwig+secure@integreat-app.de>
2020-05-29 14:21:45 +0200

    [doc] Improve push notification model documentation
    - Add docstrings to push notification model
    - Add docstrings to push notification translation model
    
    Part of #325

---
+++ src/cms/models/push_notifications/__init__.py
@@ -0,0 +1,5 @@
+"""
+This package contains both data models related to push notifications:
+:class:`~cms.models.push_notifications.push_notification.PushNotification` and
+:class:`~cms.models.push_notifications.push_notification_translation.PushNotificationTranslation`.
+"""
--- src/cms/models/push_notifications/push_notification.py
+++ src/cms/models/push_notifications/push_notification.py
@@ -1,14 +1,27 @@
-"""Model for Push Notifications
-"""
 from django.db import models
 
 
 class PushNotification(models.Model):
-    """Class representing the Push Notification base model
-
-    Args:
-        models : Databas model inherit from the standard django models
     """
+    Data model representing a push notification
+
+    :param id: The database id of the push notification
+    :param channel: The channel of the push notification
+    :param draft: Whether or not the push notification is a draft (drafts cannot be sent)
+    :param sent_date: The date and time when the push notification was sent (:obj:`None` if the push notification is
+                      not yet sent)
+    :param created_date: The date and time when the push notification was created
+    :param last_updated: The date and time when the push notification was last updated
+
+    Relationship fields:
+
+    :param region: The region of the push notification (related name: ``push_notifications``)
+
+    Reverse relationships:
+
+    :param translations: All translations of this push notification
+    """
+
     region = models.ForeignKey('Region', related_name='push_notifications', on_delete=models.CASCADE)
     channel = models.CharField(max_length=60)
     draft = models.BooleanField(default=True)
@@ -17,11 +30,28 @@ class PushNotification(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
+        """
+        This overwrites the default Python __str__ method which would return <PushNotification object at 0xDEADBEEF>
+
+        :return: The string representation of the push notification (in this case the title of the first existing
+                 push notification translation)
+        :rtype: str
+        """
         if self.translations.exists():
             return self.translations.first().title
         return ""
 
     class Meta:
+        """
+        This class contains additional meta configuration of the model class, see the
+        `official Django docs <https://docs.djangoproject.com/en/2.2/ref/models/options/>`_ for more information.
+
+        :param default_permissions: The default permissions for this model
+        :type default_permissions: tuple
+
+        :param permissions: The custom permissions for this model
+        :type permissions: tuple
+        """
         default_permissions = ()
         permissions = (
             ('view_push_notifications', 'Can view push notification'),
