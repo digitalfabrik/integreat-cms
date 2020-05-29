e740f4323737138720a24eef9a9c84b4db7360d0
Timo Ludwig <ludwig+secure@integreat-app.de>
2020-05-29 16:02:15 +0200

    [doc] Improve user model documentation
    - Add docstrings to user profile model
    - Add docstrings to user mfa model
    - Add docstrings to organization model
    
    Part of #325

--- src/cms/models/users/user_profile.py
+++ src/cms/models/users/user_profile.py
@@ -6,10 +6,21 @@ from ..regions.region import Region
 
 
 class UserProfile(models.Model):
+    """
+    Data model representing a user profile
+
+    :param id: The database id of the user profile
+
+    Relationship fields:
+
+    :param user: The user this profile belongs to (related name: ``profile``)
+    :param regions: The regions of this user (related name: ``users``)
+    :param organization: The organization of the user (related name: ``members``)
+    """
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile', on_delete=models.CASCADE)
     regions = models.ManyToManyField(Region, related_name='users', blank=True)
-    organization = models.ForeignKey(Organization, null=True, blank=True, on_delete=models.SET_NULL)
+    organization = models.ForeignKey(Organization, related_name='members', null=True, blank=True, on_delete=models.SET_NULL)
 
     @property
     def roles(self):
