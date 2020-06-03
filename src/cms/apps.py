df4921415c7b42cbc4ed45160be5e9323e22a467
Timo Ludwig <ludwig+secure@integreat-app.de>
2020-06-04 00:22:46 +0200

    [doc] Add documentation of cms app config (fixes #330)

--- src/cms/apps.py
+++ src/cms/apps.py
@@ -1,6 +1,3 @@
-"""
-Django related class representing a config of an app
-"""
 import logging
 import sys
 from django.conf import settings
@@ -10,12 +7,23 @@ logger = logging.getLogger(__name__)
 
 class CmsConfig(AppConfig):
     """
-    Class inheriting the django AppConfig
+    This class represents the Django-configuration of the backend.
+
+    See :class:`django.apps.AppConfig` for more information.
+
+    :param name: The name of the app
+    :type name: str
     """
 
     name = 'cms'
 
     def ready(self):
+        """
+        This function gets executed exactly once each time the cms starts. We use it to check wether the secret key was
+        not changed in production mode and show an error message if this is the case.
+
+        See :meth:`django.apps.AppConfig.ready` for more information.
+        """
         if settings.SECRET_KEY == '-!v282$zj815_q@htaxcubylo)(l%a+k*-xi78hw*#s2@i86@_' and not settings.DEBUG:
             logger.error("You are running the Integreat CMS in production mode. Change the SECRET_KEY in the settings.py!")
             sys.exit(1)
