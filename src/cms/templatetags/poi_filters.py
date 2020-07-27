52a381c1c92529aa736967ee95f058f3fdd26190
Timo Ludwig <ludwig+secure@integreat-app.de>
2020-07-27 08:58:23 +0200

    Remove unused POI template filters

--- src/cms/templatetags/poi_filters.py
+++ src/cms/templatetags/poi_filters.py
@@ -13,37 +13,3 @@ def poi_translation_title(poi, language):
         poi_translation = all_poi_translations.first()
         return f'{poi_translation.title} ({poi_translation.language})'
     return ''
-
-
-@register.filter
-def poi_translation_creator(poi, language):
-    all_poi_translations = poi.translations
-    poi_translation = all_poi_translations.filter(language__code=language.code)
-    if poi_translation.exists():
-        return poi_translation.first().creator
-    if all_poi_translations.exists():
-        poi_translation = all_poi_translations.first()
-        return f'{poi_translation.creator} ({poi_translation.language})'
-    return ''
-
-
-@register.filter
-def poi_translation_last_updated(poi, language):
-    all_poi_translations = poi.translations
-    poi_translation = all_poi_translations.filter(language__code=language.code)
-    if poi_translation.exists():
-        return poi_translation.first().last_updated
-    if all_poi_translations.exists():
-        return all_poi_translations.first().last_updated
-    return ''
-
-
-@register.filter
-def poi_translation_created_date(poi, language):
-    all_poi_translations = poi.translations
-    poi_translation = all_poi_translations.filter(language__code=language.code)
-    if poi_translation.exists():
-        return poi_translation.first().created_date
-    if all_poi_translations.exists():
-        return all_poi_translations.first().created_date
-    return ''
