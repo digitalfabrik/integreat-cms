9d3b1b6b5463af9dc12ed4e0a0a9119fbde6739e
Timo Ludwig <ludwig+secure@integreat-app.de>
2020-08-19 18:47:49 +0200

    Add simple view tests (fixes #471)
    - Check if all admin views and all region views return the HTTP status code 200.
    - Require static files before running tests in CircleCI

--- src/cms/tests/__init__.py
+++ src/cms/tests/__init__.py
@@ -4,3 +4,5 @@ We use unit tests to validate that the functionality of the cms works as expecte
 For more information, see :doc:`topics/testing/index` and :doc:`topics/testing/overview`.
 """
 from .pages import *
+from .views.admin_view_test import AdminViewTest
+from .views.region_view_test import RegionViewTest
---
+++ src/cms/tests/views/__init__.py
@@ -0,0 +1,5 @@
+"""
+This package contains simple unit tests for views.
+It just ensures that all views return status code 200 and
+does not check whether the response content is valid in any way.
+"""
