stanislavkocan/cms-django

Change messages for consistency, fixes #282
Co-authored-by: Amin Al-Ait <aminalait@outlook.com>

src/cms/views/events/event_view.py
@@ -165,10 +165,10 @@ class EventView(PermissionRequiredMixin, TemplateView):
             if not event_instance:
                 if published:
                     messages.success(
-                        request, _("Event was successfully created and published.")
+                        request, _("Event was successfully created and published")
                     )
                 else:
-                    messages.success(request, _("Event was successfully created."))
+                    messages.success(request, _("Event was successfully created"))
                 return redirect(
                     "edit_event",
                     **{
@@ -181,17 +181,17 @@ class EventView(PermissionRequiredMixin, TemplateView):
                 if published:
                     messages.success(
                         request,
-                        _("Event translation was successfully created and published."),
+                        _("Translation event was successfully created and published"),
                     )
                 else:
                     messages.success(
-                        request, _("Event translation was successfully created.")
+                        request, _("Translation event was successfully created")
                     )
             else:
                 if published:
-                    messages.success(request, _("Event was successfully published."))
+                    messages.success(request, _("Event was successfully published"))
                 else:
-                    messages.success(request, _("Event was successfully saved."))
+                    messages.success(request, _("Event was successfully saved"))
 
         return render(
             request,

src/cms/views/pois/poi_actions.py
@@ -24,7 +24,7 @@ def archive_poi(request, poi_id, region_slug, language_code):
     poi.archived = True
     poi.save()
 
-    messages.success(request, _("POI was successfully archived."))
+    messages.success(request, _("POI was successfully archived"))
 
     return redirect(
         "pois",
@@ -44,7 +44,7 @@ def restore_poi(request, poi_id, region_slug, language_code):
     poi.archived = False
     poi.save()
 
-    messages.success(request, _("POI was successfully restored."))
+    messages.success(request, _("POI was successfully restored"))
 
     return redirect(
         "pois",
@@ -61,7 +61,7 @@ def delete_poi(request, poi_id, region_slug, language_code):
 
     poi = POI.objects.get(id=poi_id)
     poi.delete()
-    messages.success(request, _("POI was successfully deleted."))
+    messages.success(request, _("POI was successfully deleted"))
 
     return redirect(
         "pois",

digitalfabrik/integreat-cms

Co-authored-by: Timo Ludwig <ludwig@integreat-app.de>

src/cms/views/events/event_view.py
@@ -164,10 +164,10 @@ class EventView(PermissionRequiredMixin, TemplateView):
             if not event_instance:
                 if published:
                     messages.success(
-                        request, _("Event was successfully created and published.")
+                        request, _("Event was successfully created and published")
                     )
                 else:
-                    messages.success(request, _("Event was successfully created."))
+                    messages.success(request, _("Event was successfully created"))
                 return redirect(
                     "edit_event",
                     **{
@@ -180,17 +180,17 @@ class EventView(PermissionRequiredMixin, TemplateView):
                 if published:
                     messages.success(
                         request,
-                        _("Event translation was successfully created and published."),
+                        _("Event translation was successfully created and published"),
                     )
                 else:
                     messages.success(
-                        request, _("Event translation was successfully created.")
+                        request, _("Event translation was successfully created")
                     )
             else:
                 if published:
-                    messages.success(request, _("Event was successfully published."))
+                    messages.success(request, _("Event was successfully published"))
                 else:
-                    messages.success(request, _("Event was successfully saved."))
+                    messages.success(request, _("Event was successfully saved"))
 
         return render(
             request,

src/cms/views/pois/poi_actions.py
@@ -24,7 +24,7 @@ def archive_poi(request, poi_id, region_slug, language_code):
     poi.archived = True
     poi.save()
 
-    messages.success(request, _("Location was successfully archived."))
+    messages.success(request, _("Location was successfully archived"))
 
     return redirect(
         "pois",
@@ -44,7 +44,7 @@ def restore_poi(request, poi_id, region_slug, language_code):
     poi.archived = False
     poi.save()
 
-    messages.success(request, _("Location was successfully restored."))
+    messages.success(request, _("Location was successfully restored"))
 
     return redirect(
         "pois",
@@ -61,7 +61,7 @@ def delete_poi(request, poi_id, region_slug, language_code):
 
     poi = POI.objects.get(id=poi_id)
     poi.delete()
-    messages.success(request, _("Location was successfully deleted."))
+    messages.success(request, _("Location was successfully deleted"))
 
     return redirect(
         "pois",