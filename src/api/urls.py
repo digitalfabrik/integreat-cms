9eae1e0f55a7679eb5b595f6339f43c145a24114
Sven Seeberg <mail@sven-seeberg.de>
2020-10-19 18:13:20 +0200

    Add imprint API endpoint, fixes #80
    * Add imprint API endpoint
    * Make trailing slashes optional for all API endpoints
    * Merge offer/extra and offers/extras URL patterns

--- src/api/urls.py
+++ src/api/urls.py
@@ -14,6 +14,7 @@ from .v3.feedback import (
     imprint_page_feedback,
     legacy_feedback_endpoint,
 )
+from .v3.imprint import imprint
 from .v3.languages import languages
 from .v3.pages import pages
 from .v3.push_notifications import sent_push_notifications
@@ -22,67 +23,60 @@ from .v3.offers import offers
 from .v3.single_page import single_page
 
 urlpatterns = [
-    url(r"regions/$", regions),
-    url(r"regions/live/$", liveregions),
-    url(r"regions/hidden/$", hiddenregions),
-    url(r"regions/pushnew/$", pushnew),
+    url(r"regions/?$", regions),
+    url(r"regions/live/?$", liveregions),
+    url(r"regions/hidden/?$", hiddenregions),
+    url(r"regions/pushnew/?$", pushnew),
     url(
         r"(?P<region_slug>[-\w]+)/",
         include(
             [
-                url(r"languages$", languages),
-                url(r"offers$", offers),
+                url(r"languages/?$", languages),
+                url(r"offers/?$", offers),
                 url(
-                    r"(?P<language_code>[-\w]+)/sent_push_notifications$",
+                    r"(?P<language_code>[-\w]+)/sent_push_notifications/?$",
                     sent_push_notifications,
                 ),
                 url(
-                    r"(?P<language_code>[-\w]+)/feedback$",
+                    r"(?P<language_code>[-\w]+)/feedback/?$",
                     legacy_feedback_endpoint.legacy_feedback_endpoint,
                 ),
                 url(
-                    r"(?P<language_code>[-\w]+)/feedback/page$",
+                    r"(?P<language_code>[-\w]+)/feedback/page/?$",
                     page_feedback.page_feedback,
                 ),
                 url(
-                    r"(?P<language_code>[-\w]+)/feedback/event$",
+                    r"(?P<language_code>[-\w]+)/feedback/event/?$",
                     event_feedback.event_feedback,
                 ),
                 url(
-                    r"(?P<language_code>[-\w]+)/feedback/imprint-page$",
+                    r"(?P<language_code>[-\w]+)/feedback/imprint-page/?$",
                     imprint_page_feedback.imprint_page_feedback,
                 ),
                 url(
-                    r"(?P<language_code>[-\w]+)/feedback/categories$",
+                    r"(?P<language_code>[-\w]+)/feedback/categories/?$",
                     region_feedback.region_feedback,
                 ),
                 url(
-                    r"(?P<language_code>[-\w]+)/feedback/search$",
+                    r"(?P<language_code>[-\w]+)/feedback/search/?$",
                     search_result_feedback.search_result_feedback,
                 ),
                 url(
-                    r"(?P<language_code>[-\w]+)/feedback/extras$",
+                    r"(?P<language_code>[-\w]+)/feedback/(extras|offers)/?$",
                     offer_list_feedback.offer_list_feedback,
                 ),
                 url(
-                    r"(?P<language_code>[-\w]+)/feedback/offers$",
-                    offer_list_feedback.offer_list_feedback,
-                ),
-                url(
-                    r"(?P<language_code>[-\w]+)/feedback/extra$",
+                    r"(?P<language_code>[-\w]+)/feedback/(extra|offer)/?$",
                     offer_feedback.offer_feedback,
                 ),
                 url(
-                    r"(?P<language_code>[-\w]+)/feedback/offer$",
-                    offer_feedback.offer_feedback,
-                ),
-                url(
-                    r"(?P<language_code>[-\w]+)/feedback/events$",
+                    r"(?P<language_code>[-\w]+)/feedback/events/?$",
                     event_list_feedback.event_list_feedback,
                 ),
-                url(r"(?P<language_code>[-\w]+)/pages/$", pages),
-                url(r"(?P<language_code>[-\w]+)/offers/$", offers),
-                url(r"(?P<language_code>[-\w]+)/page/$", single_page),
+                url(r"(?P<language_code>[-\w]+)/pages/?$", pages),
+                url(r"(?P<language_code>[-\w]+)/offers/?$", offers),
+                url(r"(?P<language_code>[-\w]+)/page/?$", single_page),
+                url(r"(?P<language_code>[-\w]+)/(imprint|disclaimer)/?$", imprint),
             ]
         ),
     ),
