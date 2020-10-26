634d061837de9e30523c10eeda3b96d6b03ac639
Sven Seeberg <mail@sven-seeberg.de>
2020-10-26 17:28:23 +0100

    Simple feedback list

--- src/cms/views/feedback/feedback_view.py
+++ src/cms/views/feedback/feedback_view.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from cms.forms.feedback.feedback import FeedbackForm
-
+from cms.models.feedback.event_feedback import EventFeedback
+from cms.models.feedback.offer_feedback import OfferFeedback
 
 class FeedbackView(TemplateView):
    template_name= "feedback/feedback_form.html"
    def get(self, request):
-        form = FeedbackForm()
-        return render(request, self.template_name, {'form': form} )
-       
+        feedback_events = EventFeedback.objects.filter()
+        feedback_offers = OfferFeedback.objects.filter()
+        return render(request, self.template_name, {'feedback_events': feedback_events, 'feedback_offers': feedback_offers} )
+
