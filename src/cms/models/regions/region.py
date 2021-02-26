digitalfabrik/integreat-cms

commit da858ad7199e27e49116bfc49ff722462f32ce2e

Fix FieldError in PDF API (fixes #711)
- Add possibility to return unrestricted queryset of all pages of a region
- Adapt PDF API to new concept of archived pages introduced in #598

src/cms/locale/de/LC_MESSAGES/django.po
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: 1.0\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2021-02-25 17:27+0000\n"
+"POT-Creation-Date: 2021-02-25 22:04+0000\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: Integreat <info@integreat-app.de>\n"
 "Language-Team: Integreat <info@integreat-app.de>\n"
@@ -575,7 +575,7 @@ msgstr "Einstellungen"
 #: models/languages/language_tree_node.py:37 models/offers/offer.py:24
 #: models/pages/imprint_page.py:27 models/pages/page.py:40
 #: models/pois/poi.py:18 models/push_notifications/push_notification.py:17
-#: models/regions/region.py:285
+#: models/regions/region.py:311
 msgid "region"
 msgstr "Region"
 
@@ -1394,7 +1394,7 @@ msgstr ""
 "Dies gewährt allen Benutzern dieser Region Zugang zum überregionalen Autoren-"
 "Chat."
 
-#: models/regions/region.py:287 models/users/user_profile.py:30
+#: models/regions/region.py:313 models/users/user_profile.py:30
 msgid "regions"
 msgstr "Regionen"
 

src/cms/models/regions/region.py
@@ -219,6 +219,11 @@ class Region(models.Model):
         """
         This property returns a QuerySet of all archived pages and their descendants of this region.
 
+        Per default, the returned queryset has some limitations because of the usage of
+        :meth:`~django.db.models.query.QuerySet.union`. To perform the extra effort of returning an unrestricted
+        queryset, use :meth:`~cms.models.regions.region.Region.get_pages` with the parameters ``archived`` and
+        ``return_unrestricted_queryset`` set to ``True``.
+
         :return: A QuerySet of all archived pages of this region
         :rtype: ~mptt.querysets.TreeQuerySet [ ~cms.models.pages.page.Page ]
         """
@@ -244,6 +249,12 @@ class Region(models.Model):
         A page is considered as "non-archived" if its ``explicitly_archived`` property is ``False`` and all of the
         page's ancestors are not archived as well.
 
+        Per default, the returned queryset has some limitations because of the usage of
+        :meth:`~django.db.models.query.QuerySet.difference` (see :meth:`~django.db.models.query.QuerySet.union` for some
+        restrictions). To perform the extra effort of returning an unrestricted queryset, use
+        :meth:`~cms.models.regions.region.Region.get_pages` with the parameter ``return_unrestricted_queryset`` set to
+        ``True``.
+
         :return: A QuerySet of all non-archived pages of this region
         :rtype: ~mptt.querysets.TreeQuerySet [ ~cms.models.pages.page.Page ]
         """
@@ -256,20 +267,35 @@ class Region(models.Model):
         # "recursetree" of django-mptt (see :doc:`django-mptt:templates`)
         return non_archived_pages.order_by("tree_id", "lft")
 
-    def get_pages(self, archived):
+    def get_pages(self, archived=False, return_unrestricted_queryset=False):
         """
         This method returns either all archived or all non-archived pages of this region.
-        To retrieve all pages independently from their archived-state, use the reverse foreign key ``region.pages``.
+        To retrieve all pages independently from their archived-state, use the reverse foreign key
+        :attr:`~cms.models.regions.region.Region.pages`.
 
-        :param archived: Whether or not only archived pages should be returned
+        Per default, the returned queryset has some limitations because of the usage of
+        :meth:`~django.db.models.query.QuerySet.difference` and :meth:`~django.db.models.query.QuerySet.union`.
+        To perform the extra effort of returning an unrestricted queryset, set the parameter
+        ``return_unrestricted_queryset`` to ``True``.
+
+        :param archived: Whether or not only archived pages should be returned (default: ``False``)
         :type archived: bool
+
+        :param return_unrestricted_queryset: Whether or not the result should be returned as unrestricted queryset.
+                                             (default: ``False``)
+        :type return_unrestricted_queryset: bool
 
         :return: Either the archived or the non-archived pages of this region
         :rtype: ~mptt.querysets.TreeQuerySet [ ~cms.models.pages.page.Page ]
         """
         if archived:
-            return self.archived_pages
-        return self.non_archived_pages
+            pages = self.archived_pages
+        pages = self.non_archived_pages
+        if return_unrestricted_queryset:
+            # Generate a new unrestricted queryset containing the same pages
+            page_ids = [page.id for page in pages]
+            pages = self.pages.filter(id__in=page_ids)
+        return pages
 
     def __str__(self):
         """

src/api/v3/pages.py
@@ -69,7 +69,7 @@ def pages(request, region_slug, language_code):
     """
     region = Region.get_current_region(request)
     result = []
-    for page in region.non_archived_pages:
+    for page in region.get_pages():
         page_translation = page.get_public_translation(language_code)
         if page_translation:
             result.append(transform_page(page_translation))

src/api/v3/pdf_export.py
@@ -36,6 +36,8 @@ def pdf_export(request, region_slug, language_code):
     :rtype: ~django.http.HttpResponse
     """
     region = Region.get_current_region(request)
+    # Request unrestricted queryset because pdf generator performs further operations (e.g. aggregation) on the queryset
+    pages = region.get_pages(return_unrestricted_queryset=True)
     if request.GET.get("url"):
         # remove leading and trailing slashed to avoid ambiguous urls
         url = request.GET.get("url").strip("/")
@@ -43,11 +45,9 @@ def pdf_export(request, region_slug, language_code):
         page_translation_slug = url.split("/")[-1]
         # get page by filtering for translation slug and translation language code
         page = get_object_or_404(
-            region.pages,
+            pages,
             translations__slug=page_translation_slug,
             translations__language__code=language_code,
         )
         pages = page.get_descendants(include_self=True)
-    else:
-        pages = region.pages.filter(archived=False)
     return generate_pdf(region, language_code, pages)

src/cms/views/pages/page_tree_view.py
@@ -146,7 +146,7 @@ class PageTreeView(PermissionRequiredMixin, TemplateView, PageContextMixin):
                 **context,
                 "current_menu_item": "pages",
                 "pages": pages,
-                "archived_count": region.archived_pages.count(),
+                "archived_count": region.get_pages(archived=True).count(),
                 "language": language,
                 "languages": region.languages,
                 "filter_form": filter_form,

src/sitemap/sitemaps.py
@@ -141,7 +141,7 @@ class PageSitemap(WebappSitemap):
         super().__init__(region, language)
         # Filter queryset based on region and language
         self.queryset = self.queryset.filter(
-            page__in=self.region.non_archived_pages, language=self.language
+            page__in=self.region.get_pages(), language=self.language
         )
 
 

commit 3a6b750b5f96b4bc8106b3597973a69ec367968c

Fix get_pages() for archived pages of region
region.get_pages(archived=True) returned all non-archived pages due to a bug introduced in da858ad7199e27e49116bfc49ff722462f32ce2e

src/cms/locale/de/LC_MESSAGES/django.po
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: 1.0\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2021-02-25 22:04+0000\n"
+"POT-Creation-Date: 2021-02-26 16:58+0000\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: Integreat <info@integreat-app.de>\n"
 "Language-Team: Integreat <info@integreat-app.de>\n"
@@ -575,7 +575,7 @@ msgstr "Einstellungen"
 #: models/languages/language_tree_node.py:37 models/offers/offer.py:24
 #: models/pages/imprint_page.py:27 models/pages/page.py:40
 #: models/pois/poi.py:18 models/push_notifications/push_notification.py:17
-#: models/regions/region.py:311
+#: models/regions/region.py:312
 msgid "region"
 msgstr "Region"
 
@@ -1394,7 +1394,7 @@ msgstr ""
 "Dies gewährt allen Benutzern dieser Region Zugang zum überregionalen Autoren-"
 "Chat."
 
-#: models/regions/region.py:313 models/users/user_profile.py:30
+#: models/regions/region.py:314 models/users/user_profile.py:30
 msgid "regions"
 msgstr "Regionen"
 

src/cms/models/regions/region.py
@@ -290,7 +290,8 @@ class Region(models.Model):
         """
         if archived:
             pages = self.archived_pages
-        pages = self.non_archived_pages
+        else:
+            pages = self.non_archived_pages
         if return_unrestricted_queryset:
             # Generate a new unrestricted queryset containing the same pages
             page_ids = [page.id for page in pages]