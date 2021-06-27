d10c5ea0003ed10dbfa245505c28946774d99e35
MizukiTemma <temma@integreat-app.de>
2021-06-27 18:17:35 +0200

    Autosave

--- src/frontend/js/forms/autosave.ts
+++ src/frontend/js/forms/autosave.ts
@@ -1,8 +1,11 @@
 import { getCsrfToken } from "../utils/csrf-token";
+import tinymce from "tinymce";
 
 export async function autosaveEditor() {
   const form = document.getElementById("content_form") as HTMLFormElement;
-  const formData = new FormData(form);
+  tinymce.triggerSave();
+  let formData = new FormData(form);
+  formData.append("submit_draft", "true");
   const data = await fetch(window.location.href, {
     method: "POST",
     headers: {
