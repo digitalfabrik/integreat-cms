10c9c5e6dec76c0d2528e6e8f91ad0d8b53b68fa
Giuliano Mele <mele@integreat-app.de>
2021-08-05 11:32:03 +0200

    Adds autocompletion for end date/time

---
+++ src/frontend/js/events/auto-complete.ts
@@ -0,0 +1,26 @@
+/**
+ * this file contains function for autocompletion of the event form fields
+ */
+
+window.addEventListener("load", () => {
+    if (document.getElementById("id_start_date") && document.getElementById("id_start_time")) {
+        document.getElementById("id_start_date").addEventListener("change", autocompleteEndDate);
+        document.getElementById("id_start_time").addEventListener("change", autocompleteEndTime);
+    }
+});
+
+function autocompleteEndDate(event: Event) {
+    let startDateInput = event.target as HTMLInputElement;
+    let endDateInput = document.getElementById("id_end_date") as HTMLInputElement;
+    if (endDateInput.value.length === 0) {
+        endDateInput.value = startDateInput.value;
+    }
+}
+
+function autocompleteEndTime(event: Event) {
+    let startTimeInput = event.target as HTMLInputElement;
+    let endTimeInput = document.getElementById("id_end_time") as HTMLInputElement;
+    if (endTimeInput.value.length === 0) {
+        endTimeInput.value = startTimeInput.value;
+    }
+}
