1a34501b6b1b3285392cd9a72a3b9c3929d63903
Giuliano Mele <mele@integreat-app.de>
2021-08-09 11:25:40 +0200

    Support wp api endpoints

--- src/api/urls.py
+++ src/api/urls.py
@@ -27,110 +27,95 @@ from .v3.regions import regions, liveregions, hiddenregions, pushnew
 from .v3.offers import offers
 from .v3.single_page import single_page
 
-api_urlpatterns = [
+content_api_urlpatterns = [
+    url(r"^pages/?$", pages, name="api_pages"),
+    url(r"^locations/?$", locations, name="api_locations"),
+    url(r"^events/?$", events, name="api_events"),
     url(
-        r"^(?P<region_slug>[-\w]+)/",
+        r"^page/?$",
+        single_page,
+        name="api_single_page",
+    ),
+    url(
+        r"^pdf/?$",
+        pdf_export,
+        name="api_pdf_export",
+    ),
+    url(
+        r"^sent_push_notifications/?$",
+        sent_push_notifications,
+        name="api_sent_push_notifications",
+    ),
+    url(
+        r"^(?:imprint|disclaimer)/?$",
+        imprint,
+        name="api_imprint",
+    ),
+    url(r"^(?:offers|extras)/?$", offers, name="api_offers"),
+    url(
+        r"^feedback/",
         include(
             [
-                url(r"^languages/?$", languages, name="api_languages"),
-                url(r"^(?:offers|extras)/?$", offers, name="api_offers"),
                 url(
-                    r"^(?P<language_slug>[-\w]+)/",
-                    include(
-                        [
-                            url(r"^pages/?$", pages, name="api_pages"),
-                            url(r"^locations/?$", locations, name="api_locations"),
-                            url(r"^events/?$", events, name="api_events"),
-                            url(
-                                r"^page/?$",
-                                single_page,
-                                name="api_single_page",
-                            ),
-                            url(
-                                r"^pdf/?$",
-                                pdf_export,
-                                name="api_pdf_export",
-                            ),
-                            url(
-                                r"^sent_push_notifications/?$",
-                                sent_push_notifications,
-                                name="api_sent_push_notifications",
-                            ),
-                            url(
-                                r"^(?:imprint|disclaimer)/?$",
-                                imprint,
-                                name="api_imprint",
-                            ),
-                            url(r"^(?:offers|extras)/?$", offers, name="api_offers"),
-                            url(
-                                r"^feedback/",
-                                include(
-                                    [
-                                        url(
-                                            r"^$",
-                                            legacy_feedback_endpoint.legacy_feedback_endpoint,
-                                            name="api_legacy_feedback_endpoint",
-                                        ),
-                                        url(
-                                            r"^categories/?$",
-                                            region_feedback.region_feedback,
-                                            name="api_region_feedback",
-                                        ),
-                                        url(
-                                            r"^page/?$",
-                                            page_feedback.page_feedback,
-                                            name="api_page_feedback",
-                                        ),
-                                        url(
-                                            r"^poi/?$",
-                                            poi_feedback.poi_feedback,
-                                            name="api_poi_feedback",
-                                        ),
-                                        url(
-                                            r"^event/?$",
-                                            event_feedback.event_feedback,
-                                            name="api_event_feedback",
-                                        ),
-                                        url(
-                                            r"^events/?$",
-                                            event_list_feedback.event_list_feedback,
-                                            name="api_event_list_feedback",
-                                        ),
-                                        url(
-                                            r"^imprint-page/?$",
-                                            imprint_page_feedback.imprint_page_feedback,
-                                            name="api_imprint_page_feedbacks",
-                                        ),
-                                        url(
-                                            r"^map/?$",
-                                            map_feedback.map_feedback,
-                                            name="api_map_feedback",
-                                        ),
-                                        url(
-                                            r"^search/?$",
-                                            search_result_feedback.search_result_feedback,
-                                            name="api_search_result_feedback",
-                                        ),
-                                        url(
-                                            r"^(?:extras|offers)/?$",
-                                            offer_list_feedback.offer_list_feedback,
-                                            name="api_offer_list_feedback",
-                                        ),
-                                        url(
-                                            r"^(?:extra|offer)/?$",
-                                            offer_feedback.offer_feedback,
-                                            name="api_offer_feedback",
-                                        ),
-                                    ]
-                                ),
-                            ),
-                        ]
-                    ),
+                    r"^$",
+                    legacy_feedback_endpoint.legacy_feedback_endpoint,
+                    name="api_legacy_feedback_endpoint",
+                ),
+                url(
+                    r"^categories/?$",
+                    region_feedback.region_feedback,
+                    name="api_region_feedback",
+                ),
+                url(
+                    r"^page/?$",
+                    page_feedback.page_feedback,
+                    name="api_page_feedback",
+                ),
+                url(
+                    r"^poi/?$",
+                    poi_feedback.poi_feedback,
+                    name="api_poi_feedback",
+                ),
+                url(
+                    r"^event/?$",
+                    event_feedback.event_feedback,
+                    name="api_event_feedback",
+                ),
+                url(
+                    r"^events/?$",
+                    event_list_feedback.event_list_feedback,
+                    name="api_event_list_feedback",
+                ),
+                url(
+                    r"^imprint-page/?$",
+                    imprint_page_feedback.imprint_page_feedback,
+                    name="api_imprint_page_feedbacks",
+                ),
+                url(
+                    r"^map/?$",
+                    map_feedback.map_feedback,
+                    name="api_map_feedback",
+                ),
+                url(
+                    r"^search/?$",
+                    search_result_feedback.search_result_feedback,
+                    name="api_search_result_feedback",
+                ),
+                url(
+                    r"^(?:extras|offers)/?$",
+                    offer_list_feedback.offer_list_feedback,
+                    name="api_offer_list_feedback",
+                ),
+                url(
+                    r"^(?:extra|offer)/?$",
+                    offer_feedback.offer_feedback,
+                    name="api_offer_feedback",
                 ),
             ]
         ),
     ),
 ]
+
 
 region_api_urlpatterns = [
     url(r"^$", regions, name="api_regions"),
@@ -141,8 +126,32 @@ region_api_urlpatterns = [
 
 
 urlpatterns = [
-    url(r"^api/", include(api_urlpatterns)),
     url(r"^api/regions/", include(region_api_urlpatterns)),
-    url(r"^wp-json/extensions/v3/", include(api_urlpatterns)),
     url(r"^wp-json/extensions/v3/sites/", include(region_api_urlpatterns)),
+    url(
+        r"^api/(?P<region_slug>[-\w]+)/",
+        include(
+            [
+                url(r"^languages/?$", languages, name="api_languages"),
+                url(r"^(?:offers|extras)/?$", offers, name="api_offers"),
+                url(r"^(?P<language_slug>[-\w]+)/", include(content_api_urlpatterns)),
+            ]
+        ),
+    ),
+    url(
+        r"^(?P<region_slug>[-\w]+)/",
+        include(
+            [
+                url(
+                    r"^de/wp-json/extensions/v3/languages/?$",
+                    languages,
+                    name="api_languages",
+                ),
+                url(
+                    r"^(?P<language_slug>[-\w]+)/wp-json/extensions/v3/",
+                    include(content_api_urlpatterns),
+                ),
+            ]
+        ),
+    ),
 ]
