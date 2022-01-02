78d2b62baa7412bbb7196d86a74866980d357567
Timo Ludwig <ludwig+secure@integreat-app.de>
2022-01-02 14:42:13 +0100

    Remove obsolete LINKCHECK_DISABLE_LISTENERS setting
    Since django-linkcheck version 1.9, it is no longer required to disable listeners manually during fixture loading

--- dev-tools/loadtestdata.sh
+++ dev-tools/loadtestdata.sh
@@ -9,7 +9,5 @@ source "$(dirname "${BASH_SOURCE[0]}")/_functions.sh"
 require_installed
 require_database
 
-export INTEGREAT_CMS_LINKCHECK_DISABLE_LISTENERS=1
-
 deescalate_privileges pipenv run integreat-cms-cli loaddata "${PACKAGE_DIR}/cms/fixtures/test_data.json"
 echo "✔ Imported test data" | print_success
