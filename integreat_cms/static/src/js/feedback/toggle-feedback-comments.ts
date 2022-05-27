9736e52c5e1f126002949d6d2877fae8bafe8334
David Venhoff <venhoff@integreat-app.de>
2022-05-27 18:01:39 +0200

    Fix feedback comments not being expandable

--- integreat_cms/static/src/js/feedback/toggle-feedback-comments.ts
+++ integreat_cms/static/src/js/feedback/toggle-feedback-comments.ts
@@ -1,7 +1,7 @@
 /*
  * This file provides a simple spoiler for feedback comments with hide/show functionality.
  */
-window.addEventListener('load', () => {
+window.addEventListener('icon-load', () => {
     document.querySelectorAll(".toggle-feedback-comment").forEach(toggle => toggle.addEventListener('click', ({target}) => {
         [...(target as HTMLElement).closest(".feedback-comment").children].forEach((node) => {
             (node as HTMLElement).classList.toggle("hidden");
