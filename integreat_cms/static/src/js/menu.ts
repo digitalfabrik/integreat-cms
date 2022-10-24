c46d84bf34d2ae848d04ab76aa5e69ed2a66a02c
Johannes <stock@integreat-app.de>
2022-10-24 10:52:36 +0200

    Add mobile menu to make cms responsive

---
+++ integreat_cms/static/src/js/menu.ts
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const mobile = document.querySelector("#mobile-menu");
+    const sidebar = document.querySelector("#primary-navigation");
+
+    mobile.addEventListener("click", () => {
+        sidebar.classList.toggle("-translate-x-full");
+    });
+});
