50b918becb930c7022c5661ff5d125124498ca07
Timo Ludwig <ludwig+secure@integreat-app.de>
2022-12-27 00:22:26 +0100

    Update lucide icon replacement
    Declare module and import private function instead of using the copied and pasted code

--- integreat_cms/static/src/types/index.d.ts
+++ integreat_cms/static/src/types/index.d.ts
@@ -9,3 +9,15 @@ declare module "htmldiff-js" {
     };
     export default InputMask;
 }
+
+type ReplaceElementOptions = {
+    nameAttr: string;
+    icons: { [key: string]: IconNode };
+    attrs: Record<string, string>;
+};
+
+// Since the module does not export the replaceElement function, we have to declare it here
+declare module "lucide/dist/esm/replaceElement" {
+    const replaceElement: (element: Element, { nameAttr, icons, attrs }: ReplaceElementOptions) => void;
+    export default replaceElement;
+}
