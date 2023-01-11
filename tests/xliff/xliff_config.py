620b1f0b0a93a64773351a0c345fd4b18cefc5a9
Sven Seeberg <mail@sven-seeberg.de>
2023-01-11 01:38:17 +0100

    Skip duplicate translations in XLIFF
    Co-authored-by: Timo Ludwig <timo.ludwig@tuerantuer.org>

--- tests/xliff/xliff_config.py
+++ tests/xliff/xliff_config.py
@@ -37,4 +37,29 @@ XLIFF_IMPORTS = [
             "message": 'Page "City Map" was imported without changes',
         },
     ),
+    (
+        {
+            "id": 1,
+            "file": "augsburg_de_en_1_2_willkommen.xliff",
+            "title": "Updated title",
+            "content": "<p>Updated content</p>",
+            "message": 'Page "Updated title" was imported successfully',
+        },
+        {
+            "id": 1,
+            "file": "augsburg_de_en_1_2_willkommen_conflict.xliff",
+            "title": "Updated title",
+            "content": "<p>Updated content</p>",
+            "confirmation_message": (
+                'Page <b>"Welcome"</b> from file <b>augsburg_de_en_1_2_willkommen.xliff</b> '
+                "was also translated in file <b>augsburg_de_en_1_2_willkommen_conflict.xliff</b>. "
+                "Please check which of the files contains the most recent version and upload only this file. "
+                "If you confirm this import, only the first file will be imported and the latter will be ignored."
+            ),
+            "message": (
+                'Page "Conflicting title" from the file <b>augsburg_de_en_1_2_willkommen_conflict.xliff</b> could not be imported. '
+                "Check if you have uploaded any other conflicting files for this page."
+            ),
+        },
+    ),
 ]
