303ba2cfabf6fd401e5038ce993ba05e9df7e9cf
Timo Ludwig <timo.ludwig@tuerantuer.org>
2023-01-25 17:43:24 +0100

    Migrate from `setup.cfg` to `pyproject.toml`

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Minimal setup.py for legacy builds.
-See setup.cfg for all setup configuration options.
+See pyproject.toml for all setup configuration options.
 """
 from setuptools import setup
 
