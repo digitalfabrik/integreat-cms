ef511f90fc429f52a3e37bdd298a47c83b59ee99
DigitalfabrikMember <41921676+DigitalfabrikMember@users.noreply.github.com>
2023-02-14 13:23:50 +0000

    Update documentation of commit 4b5f97e628a09774ed1a66826af9cf9a73967e02 [skip ci]

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2023.1.2',
+    VERSION: '2023.2.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
