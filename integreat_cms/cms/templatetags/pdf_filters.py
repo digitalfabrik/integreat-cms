02543f7b53fe6e50ea8398e60071c83872541470
philip <popien.philip@gmail.com>
2023-03-26 20:08:55 +0200

    Truncate long links in pdf exports to a maximum of 50 chars
    Improve truncation. Make use of djangos Truncator implementation.
    
    Add long link as a test case
    
    Co-authored-by: Timo Ludwig <timo.ludwig@tuerantuer.org>

--- integreat_cms/cms/templatetags/pdf_filters.py
+++ integreat_cms/cms/templatetags/pdf_filters.py
@@ -5,6 +5,7 @@ import re
 from html import unescape
 
 from django import template
+from django.utils.text import Truncator
 from lxml.etree import ParserError
 from lxml.html import fromstring, tostring
 
@@ -29,4 +30,31 @@ def pdf_strip_fontstyles(instance):
     for element in content.iter():
         if style := element.attrib.pop("style", None):
             element.attrib["style"] = re.sub(r"font-[a-zA-Z]+:[^;]+", "", style)
+    return unescape(tostring(content, with_tail=False).decode("utf-8"))
+
+
+@register.filter
+# pylint: disable=unused-variable
+def pdf_truncate_links(page_content, max_chars):
+    """
+    This tag returns the page content with truncated link texts.
+
+    :param page_content: The content of the page
+    :type page_content: str
+
+    :param max_chars: The maximal length of a link before it will be truncated
+    :type max_chars: int
+
+    :return: The content with truncated links
+    :rtype: str
+    """
+    try:
+        content = fromstring(page_content)
+    except ParserError:
+        return page_content
+    for elem, attrib, link, pos in content.iterlinks():
+        elem.text = " ".join(
+            Truncator(word).chars(max_chars) for word in elem.text.split(" ")
+        )
+
     return unescape(tostring(content, with_tail=False).decode("utf-8"))
