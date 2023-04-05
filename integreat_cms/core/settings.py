bcec9b30af661d7276700faf783e68fa1d8ced8d
Sven Seeberg <mail@sven-seeberg.de>
2023-04-05 16:00:05 +0200

    Add custom locale path in settings.py
    Co-authored-by: Timo Ludwig <timo.ludwig@tuerantuer.org>

--- integreat_cms/core/settings.py
+++ integreat_cms/core/settings.py
@@ -122,6 +122,10 @@ DEFAULT_REQUEST_TIMEOUT = int(
 #: Where release notes are stored
 RELEASE_NOTES_DIRS = os.path.join(BASE_DIR, "release_notes")
 
+#: Custom path for additional local translation files
+CUSTOM_LOCALE_PATH = os.environ.get(
+    "INTEGREAT_CMS_CUSTOM_LOCALE_PATH", "/etc/integreat-cms/locale"
+)
 
 ##############################################################
 # Firebase Push Notifications (Firebase Cloud Messaging FCM) #
@@ -697,7 +701,7 @@ LANGUAGES = [
 
 #: A list of directories where Django looks for translation files
 #: (see :setting:`django:LOCALE_PATHS` and :doc:`django:topics/i18n/index`)
-LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
+LOCALE_PATHS = [os.path.join(BASE_DIR, "locale"), CUSTOM_LOCALE_PATH]
 
 #: A string representing the language slug for this installation
 #: (see :setting:`django:LANGUAGE_CODE` and :doc:`django:topics/i18n/index`)
