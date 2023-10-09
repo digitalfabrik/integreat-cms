07d4076e3420bcc356f2db582712ba003a0afeca
Timo Brembeck <timo.brembeck@tuerantuer.org>
2023-10-10 01:07:25 +0200

    Improve the date display of the German locale

---
+++ integreat_cms/core/formats/__init__.py
@@ -0,0 +1,5 @@
+"""
+This package contains locale formats to override the default ones.
+Create one file ``formats.py`` per locale directory.
+See :setting:`django:FORMAT_MODULE_PATH` and :attr:`~integreat_cms.core.settings.FORMAT_MODULE_PATH` for more information.
+"""
