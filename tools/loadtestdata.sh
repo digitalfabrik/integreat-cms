283f21722b02652b803a1f06e4a623b83af33676
MizukiTemma <temma@integreat-app.de>
2023-11-15 17:57:51 +0100

    Test for media library
    Co-authored-by: David Venhoff <david.venhoff@tuerantuer.org>
    
    Update tests/cms/test_media_library.py
    
    Co-authored-by: David Venhoff <david.venhoff@tuerantuer.org>
    Co-authored-by: Tory <115008338+seluianova@users.noreply.github.com>

--- tools/loadtestdata.sh
+++ tools/loadtestdata.sh
@@ -9,7 +9,8 @@ source "$(dirname "${BASH_SOURCE[0]}")/_functions.sh"
 require_installed
 require_database
 
-cp "${PACKAGE_DIR}/static/src/logos/integreat/integreat-icon.svg" "${PACKAGE_DIR}/media/global/integreat-icon.svg"
+cp "${PACKAGE_DIR}/static/src/logos/integreat/integreat-logo.png" "${PACKAGE_DIR}/media/global/integreat-logo.png"
+cp "${PACKAGE_DIR}/static/src/logos/malte/malte-logo.png" "${PACKAGE_DIR}/media/global/malte-logo.png"
 deescalate_privileges integreat-cms-cli loaddata "${PACKAGE_DIR}/cms/fixtures/test_data.json" --verbosity "${SCRIPT_VERBOSITY}"
 
 echo "✔ Imported test data" | print_success
