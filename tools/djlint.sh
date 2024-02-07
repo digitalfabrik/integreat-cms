173dfef7a967e7284a35bf9071c07a8cc529299c
Sarah <sarah.sporck@tuerantuer.org>
2024-02-07 16:44:10 +0100

    feat: add ignore fileextension to djlint

--- tools/djlint.sh
+++ tools/djlint.sh
@@ -10,5 +10,6 @@ require_installed
 
 # Run djlint
 echo "Starting code formatting with djlint..." | print_info
-djlint --reformat --quiet --lint "${PACKAGE_DIR}"
+# exclude invalid html files until this is resolved https://github.com/djlint/djLint/issues/703
+djlint --reformat --quiet --lint "${PACKAGE_DIR}" --extend-exclude .djlintignore.html
 echo "✔ Code formatting finished" | print_success
