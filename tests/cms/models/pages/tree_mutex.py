3f438604bd65c26082495b1c0a60671405a8c2d0
Peter Nerlich <peter.nerlich@tuerantuer.org>
2024-03-25 10:47:04 +0100

    add test for tree_mutex

--- tests/cms/models/pages/tree_mutex.py
+++ tests/cms/models/pages/tree_mutex.py
@@ -4,39 +4,121 @@ Test tree mutex for page tree
 
 from __future__ import annotations
 
-import datetime
-from typing import TYPE_CHECKING
-from zoneinfo import ZoneInfo
+from threading import Thread
+from typing import Callable
 
-from integreat_cms.cms.models import Event, RecurrenceRule
+import pytest
+from django.db.utils import IntegrityError
 
-if TYPE_CHECKING:
-    from rrule import rrule
+from integreat_cms.cms.models import Page
+from integreat_cms.cms.utils.tree_mutex import tree_mutex
 
 
-class TestCreatingIcalRule:
-    """
-    Test whether to_ical_rrule_string function is calculating the rrule correctly
-    """
+@pytest.mark.django_db(transaction=True)
+def test_tree_mutex(load_test_data: None) -> None:
+    run_test(True)
 
-    test_event = Event(
-        start=datetime.datetime(2030, 1, 1, 11, 30, 0, 0, ZoneInfo("UTC")),
-        end=datetime.datetime(2030, 1, 1, 12, 30, 0, 0, ZoneInfo("UTC")),
+
+@pytest.mark.django_db(transaction=True)
+def test_rule_out_false_positive(load_test_data: None) -> None:
+    exception = None
+
+    def handle_exception(e: Exception) -> None:
+        nonlocal exception
+        exception = e
+
+    with pytest.raises((IntegrityError, AttributeError)) as exc_info:
+        run_test(use_mutex=False, handle_exception=handle_exception)
+        if exception:
+            raise exception
+
+    print(f"\nException is {repr(exc_info.value)}\n{repr(exc_info.value.args)}\n")
+
+    if isinstance(exc_info.value, AttributeError):
+        assert exc_info.value.args[0] == "'NoneType' object has no attribute 'is_descendant_of'"
+
+
+def run_test(use_mutex: bool, handle_exception: Callable | None = None) -> None:
+    one = Thread(
+        target=five_ten_five,
+        kwargs={
+            "contestant_id": 21,
+            "use_mutex": use_mutex,
+            "handle_exception": handle_exception,
+        },
+    )
+    two = Thread(
+        target=five_ten_five,
+        kwargs={
+            "contestant_id": 19,
+            "use_mutex": use_mutex,
+            "handle_exception": handle_exception,
+        },
     )
 
-    def test_api_rrule_every_year_start_date_in_the_past(self) -> None:
-        recurrence_rule = RecurrenceRule(
-            frequency="YEARLY",
-            interval=1,
-            weekdays_for_weekly=None,
-            weekday_for_monthly=None,
-            week_for_monthly=None,
-            recurrence_end_date=None,
+    print("starting threads…")
+    one.start()
+    two.start()
+
+    one.join()
+    two.join()
+    print("joined threads!")
+
+
+def five_ten_five(
+    contestant_id: int,
+    use_mutex: bool,
+    handle_exception: Callable | None = None,
+) -> None:
+    print(
+        f"running 5-10-5 on contestant #{contestant_id} {'with' if use_mutex else 'without'} tree_mutex"
+    )
+    try:
+        for i in range(5):
+            print(f"    [#{contestant_id}] {i}")
+            if use_mutex:
+                mforth(contestant_id)
+                mback(contestant_id)
+            else:
+                forth(contestant_id)
+                back(contestant_id)
+    except Exception as e:
+        if handle_exception:
+            handle_exception(e)
+        print(
+            f"failed 5-10-5 of contestant #{contestant_id} {'with' if use_mutex else 'without'} tree_mutex:\n  {repr(e)}"
         )
-        test_event = Event(
-            start=datetime.datetime(2020, 1, 1, 11, 30, 0, 0, ZoneInfo("UTC")),
-            end=datetime.datetime(2030, 1, 1, 12, 30, 0, 0, ZoneInfo("UTC")),
+    else:
+        print(
+            f"finished 5-10-5 of contestant #{contestant_id} {'with' if use_mutex else 'without'} tree_mutex"
         )
-        test_event.recurrence_rule = recurrence_rule
-        ical_rrule = recurrence_rule.to_ical_rrule_string()
-        assert ical_rrule == "DTSTART:20200101T113000\nRRULE:FREQ=YEARLY"
+
+
+@tree_mutex("page")
+def mforth(contestant_id: int) -> None:
+    forth(contestant_id)
+
+
+@tree_mutex("page")
+def mback(contestant_id: int) -> None:
+    back(contestant_id)
+
+
+def forth(contestant_id: int) -> None:
+    print(f"   moving contestant #{contestant_id}…")
+    contestant = Page.objects.get(id=contestant_id)
+    assert contestant is not None
+    other = Page.objects.get(id=20)
+    assert other is not None
+    contestant.move(other, "last-child")
+    print(f"OK moving contestant #{contestant_id}!")
+
+
+def back(contestant_id: int) -> None:
+    print(f"   moving back contestant #{contestant_id}…")
+    contestant = Page.objects.get(id=contestant_id)
+    assert contestant is not None
+    other = Page.objects.get(id=20)
+    assert other is not None
+    contestant.move(other, "right")
+    print(f"OK moving back contestant #{contestant_id}!")
