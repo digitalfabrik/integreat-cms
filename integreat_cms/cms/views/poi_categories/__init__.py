bb09e251401b75fd26bebe5db7ea40c6840110bb
Peter Nerlich <peter.nerlich@tuerantuer.org>
2024-04-07 16:40:58 +0200

    Display helpful message when deleting POICategory fails

--- integreat_cms/cms/views/poi_categories/__init__.py
+++ integreat_cms/cms/views/poi_categories/__init__.py
@@ -4,4 +4,8 @@ This package contains all views related to POI categories
 
 from __future__ import annotations
 
-from .poi_category_form_view import POICategoryCreateView, POICategoryUpdateView
+from .poi_category_form_view import (
+    POICategoryCreateView,
+    POICategoryDeleteView,
+    POICategoryUpdateView,
+)
