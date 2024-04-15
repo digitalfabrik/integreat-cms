54222e267af3e31a05933ff2863e1f29729e10d7
JoeyStk <johannes.stock@yahoo.de>
2024-04-15 10:00:03 +0200

    Add dev tool for ruff

---
+++ tools/ruff.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script can be used to format the python code with ruff.
+
+# For the transitional phase we use both pylint and ruff.
+
+# Import utility functions
+# shellcheck source=./tools/_functions.sh
+source "$(dirname "${BASH_SOURCE[0]}")/_functions.sh"
+
+require_installed
+
+# Run ruff
+echo "Starting code linting and formatting with ruff..." | print_info
+ruff check --fix "${BASE_DIR}"
+# ruff format "${BASE_DIR}"
+echo "✔ Code formatting finished" | print_success
