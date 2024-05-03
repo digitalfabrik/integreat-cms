5b00b0ee8744326876879bc3487cfec7e3763576
jarlhengstmengel <jarlgustav@hotmail.de>
2024-05-03 17:34:29 +0200

    Add has_pages_in_translation checking

--- integreat_cms/cms/views/pages/page_tree_view.py
+++ integreat_cms/cms/views/pages/page_tree_view.py
@@ -17,6 +17,7 @@ from ...forms import PageFilterForm
 from ...models import Page, PageTranslation
 from ..mixins import MachineTranslationContextMixin
 from .page_context_mixin import PageContextMixin
+from ...constants import translation_status
 
 if TYPE_CHECKING:
     from typing import Any
