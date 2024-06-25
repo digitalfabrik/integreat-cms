85ec7b77994ab8faaaecd728cd0da3854a94e774
charludo <github@charlotteharludo.com>
2024-06-25 10:26:40 +0200

    Explicitly specify vitest excludes

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,4 +6,13 @@ export default defineConfig({
             ignored: "**/.venv/**",
         },
     },
+    test: {
+        exclude: [
+            "**/node_modules/**",
+            "**/dist/**",
+            "**/cypress/**",
+            "**/.{idea,git,cache,output,temp,direnv}/**",
+            "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+        ],
+    },
 });
