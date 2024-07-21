0f80aa1c098ab71c255e9db35ab979532937bc8e
theresantonie <theresa.lang@tuerantuer.org>
2024-07-21 23:57:57 +0200

    create devtool and add release note

---
+++ tools/update_test_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Import utility functions
+# shellcheck source=./tools/_functions.sh
+source "$(dirname "${BASH_SOURCE[0]}")/_functions.sh"
+
+require_installed
+require_database
+
+deescalate_privileges integreat-cms-cli dumpdata --natural-primary --natural-foreign --exclude auth --exclude contenttypes --o test_data.json
