dab8e866a204e28e651463fe3b2e972c06124725
theresantonie <theresa.lang@tuerantuer.org>
2024-07-28 19:30:46 +0200

    update path for test_data.json

--- tools/update_test_data.sh
+++ tools/update_test_data.sh
@@ -7,4 +7,4 @@ source "$(dirname "${BASH_SOURCE[0]}")/_functions.sh"
 require_installed
 require_database
 
-deescalate_privileges integreat-cms-cli dumpdata --natural-primary --natural-foreign --exclude auth --exclude contenttypes --o test_data.json
+deescalate_privileges integreat-cms-cli dumpdata --natural-primary --natural-foreign --exclude auth --exclude contenttypes --o integreat_cms/cms/fixtures/test_data.json
