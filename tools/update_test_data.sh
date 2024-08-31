86af45787fe8d225500277d032c00127de0a00c3
Theresa Lang <165794287+theresantonie@users.noreply.github.com>
2024-08-31 20:38:06 +0200

    Update tools/update_test_data.sh
    Co-authored-by: David Venhoff <david.venhoff@tuerantuer.org>

--- tools/update_test_data.sh
+++ tools/update_test_data.sh
@@ -7,6 +7,6 @@ source "$(dirname "${BASH_SOURCE[0]}")/_functions.sh"
 require_installed
 require_database
 
-deescalate_privileges integreat-cms-cli dumpdata --natural-primary --natural-foreign --exclude auth --exclude contenttypes --o /integreat_cms/cms/fixtures/test_data.json
+deescalate_privileges integreat-cms-cli dumpdata --natural-primary --natural-foreign --exclude auth --exclude contenttypes --o "${PACKAGE_DIR}/cms/fixtures/test_data.json"
 # Format the new test data
 source "$(dirname "${BASH_SOURCE[0]}")/prettier.sh"
