b8e55d428d5cc323dbbea35301583465e88f6821
theresantonie <theresa.lang@tuerantuer.org>
2024-10-06 12:44:20 +0200

    initial test version

--- tests/cms/utils/test_cancel_translation.py
+++ tests/cms/utils/test_cancel_translation.py
@@ -1,5 +1,4 @@
 import pytest
-from _pytest.logging import LogCaptureFixture
 
 from django.test.client import Client
 from django.urls import reverse
@@ -7,18 +6,6 @@ from django.urls import reverse
 from integreat_cms.cms.models.pages.page_translation import PageTranslation
 from integreat_cms.cms.constants import translation_status
 from tests.conftest import EDITOR, MANAGEMENT, PRIV_STAFF_ROLES
-from tests.utils import get_messages, assert_no_error_messages
-
-success_messages = {
-    translation_status.UP_TO_DATE: [
-        'SUCCESS  Seite "test_translation_title" war nicht im Übersetzungsprozess',
-        'SUCCESS  Page "test_translation_title" was not in translation process.',
-    ],
-    translation_status.IN_TRANSLATION: [
-        'SUCCESS  Der Übersetzungsprozess für Seite "test_translation_title" wurde erfolgreich abgebrochen.',
-        'SUCCESS  Translation process was successfully cancelled for page "test_translation_title".',
-    ],
-}
 
 
 @pytest.mark.django_db
@@ -30,15 +17,9 @@ def test_page_bulk_actions(
     load_test_data: None,
     page_ids: list[int],
     login_role_user: tuple[Client, str],
-    caplog: LogCaptureFixture,
 ) -> None:
 
     client, _ = login_role_user
-
-    prev_status = {}
-    for page_id in page_ids:
-        translation = PageTranslation.objects.get(page__id=page_id)
-        prev_status[page_id] = translation.translation_state
 
     kwargs = {"region_slug": "augsburg", "language_slug": "de"}
     cancel_translation_process = reverse("cancel_translation_process", kwargs=kwargs)
@@ -47,19 +28,6 @@ def test_page_bulk_actions(
     )
     assert response.status_code == 302
 
-    assert_no_error_messages(caplog)
-
-    messages = get_messages(caplog)
-    assert len(messages) == len(page_ids)
-
     for page_id in page_ids:
         translation = PageTranslation.objects.get(page__id=page_id)
         assert translation.translation_state != translation_status.IN_TRANSLATION
-
-        target_messages = [
-            msg.replace("test_translation_title", translation.title)
-            for msg in success_messages[prev_status[page_id]]
-        ]
-
-        # check if one at least one of the larget messages could be found in the caplog
-        assert any(msg in messages for msg in target_messages)
