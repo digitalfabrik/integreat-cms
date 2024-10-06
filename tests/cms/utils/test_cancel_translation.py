d5666c52bcfb1919252d3c9125330fb00bf153ed
theresantonie <theresa.lang@tuerantuer.org>
2024-10-06 22:11:49 +0200

    reformat import block

--- tests/cms/utils/test_cancel_translation.py
+++ tests/cms/utils/test_cancel_translation.py
@@ -1,16 +1,15 @@
 import pytest
-
 from django.test.client import Client
 from django.urls import reverse
 
 from integreat_cms.cms.models.pages.page_translation import PageTranslation
 from integreat_cms.cms.constants import translation_status
-from tests.conftest import EDITOR, MANAGEMENT, PRIV_STAFF_ROLES
+from tests.conftest import MANAGEMENT, PRIV_STAFF_ROLES
 
 
 @pytest.mark.django_db
 @pytest.mark.parametrize(
-    "login_role_user", PRIV_STAFF_ROLES + [MANAGEMENT, EDITOR], indirect=True  # Rollen
+    "login_role_user", PRIV_STAFF_ROLES + [MANAGEMENT], indirect=True  # Rollen
 )
 @pytest.mark.parametrize("page_ids", [[30, 31]])
 def test_page_bulk_actions(
