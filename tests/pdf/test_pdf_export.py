9d2b08a49ce9be4d626681630f81f4803c72a3b5
theresantonie <theresa.lang@tuerantuer.org>
2024-10-07 11:34:45 +0200

    update hash for pdf export

--- tests/pdf/test_pdf_export.py
+++ tests/pdf/test_pdf_export.py
@@ -26,7 +26,7 @@ from django.urls import reverse
             "de",
             [1, 2, 3, 4, 5, 6, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 26, 27, 28],
             "",
-            "429b3fbce4/Integreat - Deutsch - Augsburg.pdf",
+            "09d5c07e9c/Integreat - Deutsch - Augsburg.pdf",
         ),
         (
             "en",
