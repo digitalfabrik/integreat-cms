eeced4437cc949c934177f35acfded28f9196bff
David Venhoff <david.venhoff@tuerantuer.org>
2024-10-21 19:42:03 +0200

    Small refactor in deepl_api_client

--- integreat_cms/deepl_api/apps.py
+++ integreat_cms/deepl_api/apps.py
@@ -49,34 +49,11 @@ class DeepLApiClientConfig(AppConfig):
                         auth_key=settings.DEEPL_AUTH_KEY,
                         server_url=settings.DEEPL_API_URL,
                     )
-                    self.supported_source_languages = [
-                        source_language.code.lower()
-                        for source_language in deepl_translator.get_source_languages()
-                    ]
-                    logger.debug(
-                        "Supported source languages by DeepL: %r",
-                        self.supported_source_languages,
-                    )
-                    assert self.supported_source_languages
-                    self.supported_target_languages = [
-                        target_languages.code.lower()
-                        for target_languages in deepl_translator.get_target_languages()
-                    ]
-                    logger.debug(
-                        "Supported target languages by DeepL: %r",
-                        self.supported_target_languages,
-                    )
-                    assert self.supported_target_languages
-                    usage = deepl_translator.get_usage()
-                    if usage.any_limit_reached:
-                        logger.warning("DeepL API translation limit reached")
-                    # pylint: disable=protected-access
-                    logger.info(
-                        "DeepL API is available at: %r (character usage: %s of %s)",
-                        deepl_translator._server_url,
-                        usage.character.count,
-                        usage.character.limit,
-                    )
+
+                    self.init_supported_source_languages(deepl_translator)
+                    self.init_supported_target_languages(deepl_translator)
+
+                    self.assert_usage_limit_not_reached(deepl_translator)
                 except (DeepLException, AssertionError) as e:
                     logger.error(e)
                     logger.error(
@@ -85,3 +62,53 @@ class DeepLApiClientConfig(AppConfig):
                     )
             else:
                 logger.info("DeepL API is disabled.")
+
+    def init_supported_source_languages(self, translator: Translator) -> None:
+        """
+        Requests the supported sources languages from the translator and sets them
+
+        :param translator: The deepl translator
+        """
+        self.supported_source_languages = [
+            source_language.code.lower()
+            for source_language in translator.get_source_languages()
+        ]
+        logger.debug(
+            "Supported source languages by DeepL: %r",
+            self.supported_source_languages,
+        )
+        assert self.supported_source_languages
+
+    def init_supported_target_languages(self, translator: Translator) -> None:
+        """
+        Requests the supported target languages from the translator and sets them
+
+        :param translator: The deepl translator
+        """
+        self.supported_target_languages = [
+            target_languages.code.lower()
+            for target_languages in translator.get_target_languages()
+        ]
+        logger.debug(
+            "Supported target languages by DeepL: %r",
+            self.supported_target_languages,
+        )
+        assert self.supported_target_languages
+
+    @staticmethod
+    def assert_usage_limit_not_reached(translator: Translator) -> None:
+        """
+        Requests the usage from the translator and asserts that no limit was reached
+
+        :param translator: The deepl translator
+        """
+        usage = translator.get_usage()
+        if usage.any_limit_reached:
+            logger.warning("DeepL API translation limit reached")
+        # pylint: disable=protected-access
+        logger.info(
+            "DeepL API is available at: %r (character usage: %s of %s)",
+            translator._server_url,
+            usage.character.count,
+            usage.character.limit,
+        )
