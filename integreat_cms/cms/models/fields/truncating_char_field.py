5ebec8ed62fbbf4d283fa9abbb0edc507d8eee1b
JoeyStk <johannes.stock@yahoo.de>
2024-10-27 00:16:01 +0200

    Fix data error when copying long contact

---
+++ integreat_cms/cms/models/fields/truncating_char_field.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class TruncatingCharField(models.CharField):
+    """
+    Custom model field for CharFields that need to be truncated
+    This is necessary in cases where we append a suffix and need to ensure to not exceed the limit and get a `DataError`.
+    """
+
+    def get_prep_value(self, value: str) -> str | None:
+        value = super().get_prep_value(value)
+
+        if value and len(value) > self.max_length:
+            return value[: self.max_length - 3] + "..."
+        return value
