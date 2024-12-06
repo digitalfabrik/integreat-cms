a022d8f701f047a034a908b1c5afe3a822af2eaf
JoeyStk <johannes.stock@yahoo.de>
2024-12-06 13:59:04 +0100

    [WIP] Page based access

---
+++ integreat_cms/matomo_api/utils.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from typing import Dict, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from django.db.models.query import QuerySet
+    from ..cms.models import Language, Page
+
+
+def create_translation_slugs(
+    pages: QuerySet[Page], languages: list[Language]
+) -> dict[Page, dict[str, str]]:
+    """
+    This function creates the translation slug for the matomo call. It returns then language_slug and the slug in the target translation.
+    """
+    translation_slugs: Dict[Page, Dict[str, str]] = {}
+    for page in pages:
+        translation_slugs[page] = {}
+        for language in languages:
+            if page_translation := page.get_translation(language.slug):
+                # print(page_translation.ancestor_path)
+                translation_slugs[page][language.slug] = page_translation.slug
+    return translation_slugs
