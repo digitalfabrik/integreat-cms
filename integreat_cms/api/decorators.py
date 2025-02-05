d32ec7efad2adceefd822f001e56e4a0328aae83
ulliholtgrave <uholtgrave@googlemail.com>
2025-02-05 13:51:17 +0100

    Check if Integreat Development Header is set for requests

--- integreat_cms/api/decorators.py
+++ integreat_cms/api/decorators.py
@@ -175,6 +175,7 @@ def matomo_tracking(func: Callable) -> Callable:
             not settings.MATOMO_TRACKING
             or not request.region.matomo_id
             or not request.region.matomo_token
+            or "HTTP_X_INTEGREAT_DEVELOPMENT" in request.META
         ):
             return func(request, *args, **kwargs)
         data = {
