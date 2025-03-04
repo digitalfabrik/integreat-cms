e19c4174244926b79c43fdd779ae97b5eda98f48
jarlhengstmengel <jarlgustav@hotmail.de>
2025-03-04 15:26:51 +0000

    Add beta permission to user

--- integreat_cms/cms/migrations/0117_add_beta_permission.py
+++ integreat_cms/cms/migrations/0117_add_beta_permission.py
@@ -40,4 +40,22 @@ class Migration(migrations.Migration):
         ("cms", "0116_remove_poi_email_remove_poi_phone_number_and_more"),
     ]
 
-    operations = [migrations.RunPython(update_roles, migrations.RunPython.noop)]
+    operations = [
+        migrations.AlterModelOptions(
+            name="user",
+            options={
+                "default_permissions": ("change", "delete", "view"),
+                "ordering": ["username"],
+                "permissions": (
+                    ("view_translation_report", "view_translation_report"),
+                    ("view_broken_links", "view_broken_links"),
+                    ("view_statistics", "view_statistics"),
+                    ("manage_translations", "manage_translations"),
+                    ("test_beta_features", "test_beta_features"),
+                ),
+                "verbose_name": "user",
+                "verbose_name_plural": "users",
+            },
+        ),
+        migrations.RunPython(update_roles, migrations.RunPython.noop)
+    ]
