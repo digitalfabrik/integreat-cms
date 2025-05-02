3f56883524e60975165fc464d6fb1caf7e1cd185
Johannes <johannes.stock@yahoo.de>
2025-05-02 16:56:56 +0200

    Add option to archive push notifications

--- integreat_cms/cms/views/push_notifications/__init__.py
+++ integreat_cms/cms/views/push_notifications/__init__.py
@@ -4,5 +4,13 @@ This package contains all views related to push notifications
 
 from __future__ import annotations
 
+from .push_notification_actions import (
+    archive_push_notification,
+    restore_push_notification,
+)
+from .push_notification_bulk_actions import (
+    ArchivePushNotificationsBulkAction,
+    RestorePushNotificationsBulkAction,
+)
 from .push_notification_form_view import PushNotificationFormView
 from .push_notification_list_view import PushNotificationListView
