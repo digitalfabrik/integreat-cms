64158f19fb0ed0fd7238aa3ff497c960beccb158
jarlhengstmengel <jarlgustav@hotmail.de>
2025-05-24 19:45:01 +0000

    Add dictionary conversion

--- integreat_cms/cms/views/statistics/statistics_actions.py
+++ integreat_cms/cms/views/statistics/statistics_actions.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 import logging
 from datetime import date, timedelta
-from typing import TYPE_CHECKING
+from typing import Any, TYPE_CHECKING
 
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
@@ -149,46 +149,60 @@ def get_page_accesses_ajax(request: HttpRequest, region_slug: str) -> JsonRespon
             status=400,
         )
 
-    try:
-        result = load_page_accesses(
-            start_date=statistics_form.cleaned_data["start_date"],
-            end_date=statistics_form.cleaned_data["end_date"],
-            period=statistics_form.cleaned_data["period"],
-            region=region,
-        )
-    except TimeoutError:
-        logger.exception("Timeout during request to Matomo API")
-        return JsonResponse(
-            {"error": "Timeout during request to Matomo API"}, status=504
-        )
-    except MatomoException:
-        logger.exception("The request to the Matomo API failed.")
-        return JsonResponse(
-            {"error": "The request to the Matomo API failed."}, status=500
-        )
-
     region_pages = Page.objects.filter(region=region)
-    languages = region.language_tree
-    region.get_page_accesses_by_language(
+    languages = region.languages
+    page_accesses = region.get_page_accesses_by_language(
         pages=region_pages,
         start_date=statistics_form.cleaned_data["start_date"],
         end_date=statistics_form.cleaned_data["end_date"],
         languages=languages,
     )
 
-    return JsonResponse(result, safe=False)
+    language_slug_table = Language.objects.only("id", "slug")
+
+    page_accesses_dict: dict[Any, Any] = {}
+    for access in page_accesses:
+        page_id = str(access["page_id"])
+        language_slug = language_slug_table.filter(id=access["language_id"]).values(
+            "slug"
+        )[0]["slug"]
+        if page_id in page_accesses_dict:
+            if language_slug in page_accesses_dict[page_id]:
+                page_accesses_dict[page_id][language_slug][
+                    access["access_date"].strftime("%Y-%m-%d")
+                ] = access["accesses"]
+            else:
+                page_accesses_dict[page_id][language_slug] = {
+                    access["access_date"].strftime("%Y-%m-%d"): access["accesses"]
+                }
+        else:
+            page_accesses_dict[page_id] = {
+                language_slug: {
+                    access["access_date"].strftime("%Y-%m-%d"): access["accesses"]
+                }
+            }
+
+    return JsonResponse(page_accesses_dict, safe=False)
 
 
-def load_page_accesses(start_date: date, end_date: date, period: str, region: Region):
+def save_page_accesses(start_date: date, end_date: date, period: str, region: Region):
     """
     Load page accesses from Matomo and save them to page accesses model
+
+    :param start_date: Earliest date
+    :param end_date: Latest date
+    :param period: The period (one of :attr:`~integreat_cms.cms.constants.matomo_periods.CHOICES`)
+    :param region: The region for which we want our page based accesses
     """
+    # Get page accesses from the matomo api client
     result = region.statistics.get_page_accesses(
         start_date=start_date,
         end_date=end_date,
         period=period,
         region=region,
     )
+
+    # Create PageAccess Objects
     accesses = [
         PageAccesses(
             access_date=access_date,
@@ -206,4 +220,3 @@ def load_page_accesses(start_date: date, end_date: date, period: str, region: Re
         unique_fields=["page", "language", "access_date"],
         update_fields=["accesses"],
     )
-    return result
