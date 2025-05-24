973b515b923e64f187d5b2e7c9dbae31c85acc84
Johannes <johannes.stock@yahoo.de>
2025-05-24 11:04:56 +0200

    Fix migration order

--- integreat_cms/cms/migrations/0134_add_opening_hours_for_contacts.py
+++ integreat_cms/cms/migrations/0134_add_opening_hours_for_contacts.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("cms", "0129_remove_region_zammad_chat_handlers_and_more"),
+        ("cms", "0133_add_page_accesses"),
     ]
 
     operations = [
