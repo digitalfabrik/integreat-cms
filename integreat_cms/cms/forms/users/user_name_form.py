9569900ec62b8c429d72e3be7a9f17758b4d6908
Waleed Binsaad <waleed.binsaad0@gmail.com>
2025-06-01 19:42:11 +0000

    Add UserNameForm to user settings

---
+++ integreat_cms/cms/forms/users/user_name_form.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import logging
+
+from django.contrib.auth import get_user_model
+
+from ..custom_model_form import CustomModelForm
+
+logger = logging.getLogger(__name__)
+
+
+class UserNameForm(CustomModelForm):
+    """
+    Form for modifying user name addresses
+    """
+
+    class Meta:
+        """
+        This class contains additional meta configuration of the form class, see the :class:`django.forms.ModelForm`
+        for more information.
+        """
+
+        #: The model of this :class:`django.forms.ModelForm`
+        model = get_user_model()
+        #: The fields of the model which should be handled by this form
+        fields = ["first_name", "last_name"]
