bacfefed34f48e74b3c6ff20772de5af18bf3c92
Waleed Binsaad <waleed.binsaad0@gmail.com>
2025-06-07 22:08:27 +0000

    Update username form and settings template

--- integreat_cms/cms/forms/users/user_name_form.py
+++ integreat_cms/cms/forms/users/user_name_form.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import logging
 
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 from ..custom_model_form import CustomModelForm
 
@@ -11,16 +12,33 @@ logger = logging.getLogger(__name__)
 
 class UserNameForm(CustomModelForm):
     """
-    Form for modifying user name addresses
+    Form for modifying user name including username, first name, and last name.
     """
 
     class Meta:
         """
-        This class contains additional meta configuration of the form class, see the :class:`django.forms.ModelForm`
-        for more information.
+        This class contains additional meta configuration of the form class,
+        see the :class:`django.forms.ModelForm` for more information.
         """
 
         #: The model of this :class:`django.forms.ModelForm`
         model = get_user_model()
         #: The fields of the model which should be handled by this form
-        fields = ["first_name", "last_name"]
+        fields = ["username", "first_name", "last_name"]
+
+    def clean_username(self) -> str:
+        """
+        Ensure that the username is unique across all users except the one being edited.
+
+        :return: The validated username
+        """
+        username = self.cleaned_data.get("username")
+        if (
+            username
+            and get_user_model()
+            .objects.filter(username=username)
+            .exclude(pk=self.instance.pk)
+            .exists()
+        ):
+            self.add_error("username", _("This username is already taken."))
+        return username
