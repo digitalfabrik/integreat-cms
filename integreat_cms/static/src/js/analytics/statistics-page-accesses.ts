5739357ea24ab879b1a1ab24519d29d5779c90e0
jarlhengstmengel <jarlgustav@hotmail.de>
2025-08-21 12:55:14 +0000

    Rework page accesses typescript

--- integreat_cms/static/src/js/analytics/statistics-page-accesses.ts
+++ integreat_cms/static/src/js/analytics/statistics-page-accesses.ts
@@ -11,6 +11,7 @@ export type Access = {
 };
 
 let chart: Chart | null = null;
+let loaderIsHidden : boolean;
 
 /* Loop over the object containing the accesses over time and count them to a total */
 const countAccesses = (accessesOverTime: object): number => {
@@ -40,12 +41,40 @@ const setAccessesPerLanguage = (
         `${accesses} ${childElement.getAttribute("data-access-translation")} (${roundedPercentage} %)`;
 };
 
-/* This function hides the loader icon once the data is there */
-const hideLoader = () => {
+/* This function ensures that when there are no accesses in the selected timeframe and languages, the AccessesBar gets updatet */
+const resetAccessesBar = (
+    accessFields: HTMLCollectionOf<Element>,
+    isEmpty : boolean
+) => {
+    if (isEmpty) {
+        Array.from(accessFields).forEach((accessField) => {
+            const allAccessesField = Array.from(accessField.parentElement?.children || []).find(
+                (el) => el !== accessField && el.classList.contains("total-accesses")
+            );
+            const editableAllAccessField = allAccessesField;
+            editableAllAccessField.textContent = `${String(0)} ${editableAllAccessField.getAttribute("data-translation")}`;
+            accessField.classList.add("hidden");
+        });
+    } else if (!isEmpty) {
+        Array.from(accessFields).forEach((accessField) => {
+            accessField.classList.remove("hidden");
+        });
+    }
+}
+
+/* This function toggles the loader icon */
+const toggleLoader = (hidden: boolean): boolean => {
     const loaderIcons = document.getElementsByClassName("page-accesses-loading");
+    if (hidden) {
+        Array.from(loaderIcons).forEach((loaderIcon) => {
+            loaderIcon.classList.remove("hidden");
+        });
+        return false;
+    }
     Array.from(loaderIcons).forEach((loaderIcon) => {
         loaderIcon.classList.add("hidden");
     });
+    return true;
 };
 
 /* Set the selected date at the top of the table */
@@ -58,13 +87,7 @@ const setDates = () => {
 
 /* The main function which updates the accesses */
 const updatePageAccesses = async (): Promise<void> => {
-    const pageAccessesNetworkError = document.getElementById("page-accesses-network-error");
-
     let parameters = {};
-
-    const HTTP_STATUS_OK = 200;
-    const HTTP_STATUS_BAD_REQUEST = 400;
-    const HTTP_STATUS_GATEWAY_TIMEOUT = 504;
 
     const statisticsForm = document.getElementById("statistics-form") as HTMLFormElement;
 
@@ -77,90 +100,84 @@ const updatePageAccesses = async (): Promise<void> => {
 
     const pageAccessesURL = document.getElementById("statistics-page-access").getAttribute("data-page-accesses-url");
     const accessFields = document.getElementsByClassName("accesses");
-    try {
-        const response = await fetch(pageAccessesURL, parameters);
-        if (response.status === HTTP_STATUS_OK) {
-            const data = (await response.json()) as AjaxResponse;
-            /* data is the nested response we get from our client. On the first level it contains the page_id and an object.
-            This object in itself contains the language_slug and another object. On the third level this object contains the accesses over time with
-            a date and the according accesses. */
-            const languageSlugToDatasetId: Map<string, number> = new Map();
-            Object.entries(data).forEach((values) => {
-                // We're on the first level of our data. Here we have the page_id and the rest of the object.
-                Array.from(accessFields).forEach((accessField) => {
-                    const id = values[0];
-                    const pageId = accessField.parentElement.getAttribute("id").replace("page-", "");
-                    // pageId is the id of a page in our page tree. Id is the id in our data object. In the next line we compare them, to find the right
-                    // data for the right row.
-                    if (id === pageId) {
-                        const accesses = values[1];
-                        // We get the column 'total accesses' in our table
-                        const allAccessesField = Array.from(accessField.parentElement?.children || []).find(
-                            (el) => el !== accessField && el.classList.contains("total-accesses")
+    const response = await fetch(pageAccessesURL, parameters);
+    const data = (await response.json()) as AjaxResponse;
+    const isEmpty = Object.keys(data).length === 0;
+    resetAccessesBar(accessFields, isEmpty)
+    /* data is the nested response we get from our client. On the first level it contains the page_id and an object.
+    This object in itself contains the language_slug and another object. On the third level this object contains the accesses over time with
+    a date and the according accesses. */
+    const languageSlugToDatasetId: Map<string, number> = new Map();
+    Object.entries(data).forEach((values) => {
+        // We're on the first level of our data. Here we have the page_id and the rest of the object.
+        Array.from(accessFields).forEach((accessField) => {
+            const id = values[0];
+            const pageId = accessField.parentElement.getAttribute("id").replace("page-", "");
+            // pageId is the id of a page in our page tree. Id is the id in our data object. In the next line we compare them, to find the right
+            // data for the right row.
+            if (id === pageId) {
+                const accesses = values[1];
+                // We get the column 'total accesses' in our table
+                const allAccessesField = Array.from(accessField.parentElement?.children || []).find(
+                    (el) => el !== accessField && el.classList.contains("total-accesses")
+                );
+                const editableAllAccessField = allAccessesField;
+                let allAccesses: number = 0;
+                // The next part goes through the subobject and the subobject. It counts the sum of accesses
+                // for all languages and all accesses for the selected timeframe
+                Object.entries(accesses).forEach((access) => {
+                    const languageSlug = access[0];
+                    if (languageSlugToDatasetId.get(languageSlug) === undefined) {
+                        const fullLabel = document
+                            .querySelector(`#chart-legend [data-language-slug="${languageSlug}"]`)
+                            .getAttribute("data-chart-item");
+                        const datasetId = chart.data.datasets.findIndex(
+                            (dataset) => dataset.label === fullLabel
                         );
-                        const editableAllAccessField = allAccessesField;
-                        let allAccesses: number = 0;
-                        // The next part goes through the subobject and the subobject. It counts the sum of accesses
-                        // for all languages and all accesses for the selected timeframe
-                        Object.entries(accesses).forEach((access) => {
-                            const languageSlug = access[0];
-                            if (languageSlugToDatasetId.get(languageSlug) === undefined) {
-                                const fullLabel = document
-                                    .querySelector(`#chart-legend [data-language-slug="${languageSlug}"]`)
-                                    .getAttribute("data-chart-item");
-                                const datasetId = chart.data.datasets.findIndex(
-                                    (dataset) => dataset.label === fullLabel
-                                );
-                                languageSlugToDatasetId.set(languageSlug, datasetId);
-                            }
-                            if (chart.isDatasetVisible(languageSlugToDatasetId.get(languageSlug))) {
-                                const accessesOverTime = access[1];
-                                allAccesses += countAccesses(accessesOverTime);
-                            }
-                        });
-                        editableAllAccessField.textContent = `${String(allAccesses)} ${editableAllAccessField.getAttribute("data-translation")}`;
-                        // Go to the object on the second level. Set the bar of accesses for every language for the selected timeframe.
-                        Object.entries(accesses).forEach((access) => {
-                            const languageSlug = access[0];
-                            const accessesOverTime = access[1];
-                            if (chart.isDatasetVisible(languageSlugToDatasetId.get(languageSlug))) {
-                                setAccessesPerLanguage(accessField, languageSlug, accessesOverTime, allAccesses);
-                            } else {
-                                setAccessesPerLanguage(accessField, languageSlug, {}, allAccesses);
-                            }
-                        });
+                        languageSlugToDatasetId.set(languageSlug, datasetId);
+                    }
+                    if (chart.isDatasetVisible(languageSlugToDatasetId.get(languageSlug))) {
+                        const accessesOverTime = access[1];
+                        allAccesses += countAccesses(accessesOverTime);
                     }
                 });
-            });
-        } else if (response.status === HTTP_STATUS_BAD_REQUEST) {
-            Array.from(accessFields).forEach((accessField) => {
-                const editableAllAccessField = accessField;
-                editableAllAccessField.textContent = accessField.getAttribute("data-client-error");
-            });
-        } else if (response.status === HTTP_STATUS_GATEWAY_TIMEOUT) {
-            Array.from(accessFields).forEach((accessField) => {
-                const editableAllAccessField = accessField;
-                editableAllAccessField.textContent = accessField.getAttribute("data-server-error");
-            });
-        }
-    } catch (error) {
-        console.error("Network error during fetch:", error);
-        pageAccessesNetworkError.classList.remove("hidden");
-    } finally {
-        hideLoader();
-        setDates();
-    }
+                editableAllAccessField.textContent = `${String(allAccesses)} ${editableAllAccessField.getAttribute("data-translation")}`;
+                // Go to the object on the second level. Set the bar of accesses for every language for the selected timeframe.
+                Object.entries(accesses).forEach((access) => {
+                    const languageSlug = access[0];
+                    const accessesOverTime = access[1];
+                    if (chart.isDatasetVisible(languageSlugToDatasetId.get(languageSlug))) {
+                        setAccessesPerLanguage(accessField, languageSlug, accessesOverTime, allAccesses);
+                    } else {
+                        setAccessesPerLanguage(accessField, languageSlug, {}, allAccesses);
+                    }
+                });
+            }
+        });
+    });
+    loaderIsHidden = toggleLoader(loaderIsHidden);
+    setDates();
 };
 
 window.addEventListener("load", async () => {
     if (document.getElementById("statistics-page-access")) {
         updatePageAccesses();
         chart = Chart.instances[0];
-        // Refresh the statistics every time a new timeframe or language is selected
-        const oldUpdate = chart.update;
-        chart.update = async () => {
-            oldUpdate.call(chart);
-            updatePageAccesses();
-        };
+        // Set event handler for updating Page Accesses when date is changed
+        const statisticsForm = document.getElementById("statistics-form") as HTMLFormElement;
+        statisticsForm?.addEventListener("submit", async (event: Event) => {
+            // Prevent form submit
+            event.preventDefault();
+            loaderIsHidden = toggleLoader(loaderIsHidden);
+            await updatePageAccesses();
+        });
+
+        // Set event handler for updating Page Accesses when selected languages change
+        document.addEventListener("change", async (event) => {
+            if ((event.target as HTMLInputElement).type === 'checkbox') {
+                loaderIsHidden = toggleLoader(loaderIsHidden);
+                await updatePageAccesses();
+            }
+        });
     }
 });
