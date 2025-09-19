eed5f98d3b6de399221f243177720646c02a6636
MizukiTemma <82345046+MizukiTemma@users.noreply.github.com>
2025-09-19 13:57:48 +0200

    Update integreat_cms/cms/models/pages/page.py
    Co-authored-by: Peter Nerlich <PeterNerlich@users.noreply.github.com>

--- integreat_cms/cms/models/pages/page.py
+++ integreat_cms/cms/models/pages/page.py
@@ -437,7 +437,7 @@ class Page(AbstractTreeNode, AbstractBasePage):
                 results.append(
                     format_object_translation(page_translation, "page", language_slug),
                 )
-                title_already_in_result += [page_translation.title]
+                title_already_in_result.append(page_translation.title)
 
         return results
 
