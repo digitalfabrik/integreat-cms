3ee97b1d31f141ee2b7ab41f1c0cddf1b2c062b3
MizukiTemma <temma@integreat-app.de>
2025-09-30 12:18:21 +0200

    Fix initializing of set

--- integreat_cms/cms/models/pages/page.py
+++ integreat_cms/cms/models/pages/page.py
@@ -419,7 +419,7 @@ class Page(AbstractTreeNode, AbstractBasePage):
         link_suggestion_flag = kwargs["link_suggestion_flag"]
 
         pages = region.pages.all().cache_tree(archived=archived_flag)
-        title_already_in_result: set[str] = {}
+        title_already_in_result: set[str] = set()
 
         for page in pages:
             page_translation = page.get_translation(language_slug)
