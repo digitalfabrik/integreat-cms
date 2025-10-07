16ac37474dff926c991be6393a47343722f3e6c2
Peter Nerlich <peter.nerlich@tuerantuer.org>
2025-10-07 19:01:07 +0200

    Log shortcode errors

--- integreat_cms/cms/utils/shortcodes/__init__.py
+++ integreat_cms/cms/utils/shortcodes/__init__.py
@@ -2,6 +2,7 @@
 This module contains implementations for the shortcodes content filters
 """
 
+import logging
 from typing import Any
 
 import shortcodes
@@ -10,6 +11,9 @@ from django.template.defaultfilters import stringfilter
 
 from .contact import contact
 from .page import page
+
+
+logger = logging.getLogger(__name__)
 
 register = template.Library()
 
@@ -26,7 +30,9 @@ parser = shortcodes.Parser(start="[", end="]", esc="\\", ignore_unknown=True)
 def expand_shortcodes(content: str, context: dict[str, Any] | None = None) -> str:
     try:
         return parser.parse(content, context)
-    except shortcodes.ShortcodeError:
+    except shortcodes.ShortcodeError as e:
+        n = "\n"
+        logger.warning(f"Failed expanding shortcodes:  {e}{n}context: {context!r}", exc_info=True)
         # We failed expanding the shortcodes,
         # the best way we can fail gracefully is to just return the original content
         return content
