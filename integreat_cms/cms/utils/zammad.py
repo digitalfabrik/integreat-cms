cbb0b6a38b5b0a8c8ef920d47f3446e33a015846
Sven Seeberg <mail@sven-seeberg.de>
2025-12-04 14:04:14 +0100

    Update integreat_cms/cms/utils/zammad.py
    Co-authored-by: JoeyStk <72705147+JoeyStk@users.noreply.github.com>

--- integreat_cms/cms/utils/zammad.py
+++ integreat_cms/cms/utils/zammad.py
@@ -216,7 +216,7 @@ class ZammadAPI:
             )
         except ValueError:
             return None
-        if response.status_code < 200 or response.status_code > 299:
+        if response.ok:
             return None
         return response.json()
 
