a3c7c74b446fa38a8365d87853d49aff5bc4d026
Jonas Buchholz <jonas.buchholz@altaia.net>
2026-01-27 14:57:32 +0100

    add search_fields description

--- integreat_cms/cms/forms/contacts/contact_search_form.py
+++ integreat_cms/cms/forms/contacts/contact_search_form.py
@@ -5,7 +5,7 @@ from ...search.search_fields import CONTACT_SEARCH_FIELDS
 
 
 class ContactSearchForm(ObjectSearchForm):
-    search_fields = CONTACT_SEARCH_FIELDS.keys()
+    search_fields = CONTACT_SEARCH_FIELDS
 
     def apply_filters(self, queryset: QuerySet) -> QuerySet:
         return super().apply_filters(queryset).distinct()
