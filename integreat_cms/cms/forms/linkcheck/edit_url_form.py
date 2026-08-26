4f14bdb80215f04345ff6c5e6a512561f6151ce3
MizukiTemma <temma@integreat-app.de>
2026-08-26 16:28:09 +0200

    Fix merge conflicts

--- integreat_cms/cms/forms/linkcheck/edit_url_form.py
+++ integreat_cms/cms/forms/linkcheck/edit_url_form.py
@@ -1,39 +1,24 @@
 from __future__ import annotations
 
 import logging
+import re
 
 from django import forms
 from django.core.validators import EmailValidator, URLValidator
 from django.utils.translation import gettext_lazy as _
 
+from ...utils.link_utils import format_phone_number
+
 logger = logging.getLogger(__name__)
 
 
-class LinkField(forms.URLField):
+class LinkField(forms.CharField):
     """
     A field for links that might be URLs but could also be mailto: or tel: links
     """
 
     #: Disable the default URL validator
     default_validators: list[URLValidator | EmailValidator] = []
-    #: Whether to skip the validation URL fragments in URLField.to_python()
-    skip_url_fragment_validation: bool = True
-
-    def to_python(self, value: str) -> str:
-        """
-        Convert the string value to the appropriate Python data structure for this field
-
-        :param value: The value that was input into the form
-        :returns: The Python value
-        """
-        if self.skip_url_fragment_validation:
-            # Skip the URL field to_python for email and phone links
-            logger.debug(
-                "Value %r is a mailto or tel link, skipping to_python() of URLField.",
-                value,
-            )
-            return super(forms.URLField, self).to_python(value)
-        return super().to_python(value)
 
     def clean(self, value: str) -> str:
         """
@@ -43,8 +28,11 @@ class LinkField(forms.URLField):
         :param value: The value that was input into the form
         :returns: The cleaned value
         """
-        if value.startswith("mailto:"):
-            email = value[7:]
+        if "@" in value:
+            email = value
+            if value.startswith("mailto:"):
+                email = value[7:]
+
             logger.debug(
                 "Value %r is an email link, enforcing EmailValidator on %r",
                 value,
@@ -53,10 +41,18 @@ class LinkField(forms.URLField):
             self.validators.append(EmailValidator())
             self.error_messages["invalid"] = _("Enter a valid email address.")
             return f"mailto:{super().clean(email)}"
-        if not value.startswith("tel:"):
-            logger.debug("Value %r is a normal link, enforcing URLValidator", value)
-            self.validators.append(URLValidator(schemes=["http", "https"]))
-            self.skip_url_fragment_validation = False
+
+        if not value.startswith("tel:") and re.fullmatch(r"[+\d][\d ]*", value):
+            formatted_phone_number = format_phone_number(value)
+            logger.debug(
+                "Value %r looks like an phone link, formatting to %r",
+                value,
+                formatted_phone_number,
+            )
+            return f"tel:{formatted_phone_number}"
+
+        logger.debug("Value %r is a normal link, enforcing URLValidator", value)
+        self.validators.append(URLValidator(schemes=["http", "https"]))
         return super().clean(value)
 
 
